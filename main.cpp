ad991c58a78606db7b5c43889f99b259f6af8cf0
Jan Christian Kruse <jck@maxr.org>
2007-10-01 18:27:31 +0000

    git-svn-id: svn://maxr.org/MAX/MAX@17 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- main.cpp
+++ main.cpp
@@ -62,6 +62,7 @@ int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrevInstance,LPSTR args,INT)
 	// SplashScreen anzeigen
 	buffer=SDL_LoadBMP("InitPopup.bmp");
 	SDL_BlitSurface(buffer,NULL,screen,NULL);
+	SDL_WM_SetIcon(SDL_LoadBMP("MaxIcon.bmp"), NULL); //JCK
 	screen=SDL_SetVideoMode(500,420,32,SDL_HWSURFACE|SDL_NOFRAME);
 	SDL_UpdateRect(screen,0,0,0,0);
 	SDL_WM_SetCaption("MMs M.A.X. by DoctorDeath",NULL);
