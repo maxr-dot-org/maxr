502902013abc6342dfa688840d35de82f026eb48
Bernd Kosmahl <beko@maxr.org>
2007-10-02 22:41:44 +0000

    formatiert
    git-svn-id: svn://maxr.org/MAX/MAX@78 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- sound.cpp
+++ sound.cpp
@@ -21,96 +21,107 @@ int VoiceChannel=VOICE_CHANNEL_MIN;
 int SoundLoopChannel=6;
 
 // Lokale Prototypen /////////////////////////////////////////////////////////
-void MusicFinished(void);
+void MusicFinished ( void );
 
 // Funktionen ////////////////////////////////////////////////////////////////
 // Initialisiert den Sound:
-int InitSound(int frequency,int chunksize){
+int InitSound ( int frequency,int chunksize )
+{
 	int audio_rate,audio_channels;
 	Uint16 audio_format;
 
-  // SDL Mixer initialisieren:
-	int haha = Mix_OpenAudio(frequency,AUDIO_S16,2,chunksize);
-  if(haha<0)return 0;
-	Mix_QuerySpec(&audio_rate, &audio_format, &audio_channels);
+	// SDL Mixer initialisieren:
+	int haha = Mix_OpenAudio ( frequency,AUDIO_S16,2,chunksize );
+	if ( haha<0 ) return 0;
+	Mix_QuerySpec ( &audio_rate, &audio_format, &audio_channels );
 
-  // Callback für Musik installieren:
-  Mix_HookMusicFinished(MusicFinished);
+	// Callback für Musik installieren:
+	Mix_HookMusicFinished ( MusicFinished );
 
-  return 1;
+	return 1;
 }
 
 // Schließt den Sound:
-void CloseSound(void){
-  if(!sound)return;
+void CloseSound ( void )
+{
+	if ( !sound ) return;
 	Mix_CloseAudio();
 	return;
 }
 
 // Spielt einen Voice-Sound:
-void PlayVoice(sSOUND *snd){
-  if(!sound||VoiceMute)return;
-  Mix_PlayChannel(VoiceChannel,snd,0);
-  Mix_Volume(VoiceChannel,VoiceVol);
-  VoiceChannel++;
-  if(VoiceChannel>VOICE_CHANNEL_MAX)VoiceChannel=VOICE_CHANNEL_MIN;
+void PlayVoice ( sSOUND *snd )
+{
+	if ( !sound||VoiceMute ) return;
+	Mix_PlayChannel ( VoiceChannel,snd,0 );
+	Mix_Volume ( VoiceChannel,VoiceVol );
+	VoiceChannel++;
+	if ( VoiceChannel>VOICE_CHANNEL_MAX ) VoiceChannel=VOICE_CHANNEL_MIN;
 }
 
 // Spielt einen FX-Sound:
-void PlayFX(sSOUND *snd){
-  if(!sound||SoundMute)return;
-  Mix_PlayChannel(SoundChannel,snd,0);
-  Mix_Volume(SoundChannel,SoundVol);
-  SoundChannel++;
-  if(SoundChannel>SOUND_CHANNEL_MAX)SoundChannel=SOUND_CHANNEL_MIN;
+void PlayFX ( sSOUND *snd )
+{
+	if ( !sound||SoundMute ) return;
+	Mix_PlayChannel ( SoundChannel,snd,0 );
+	Mix_Volume ( SoundChannel,SoundVol );
+	SoundChannel++;
+	if ( SoundChannel>SOUND_CHANNEL_MAX ) SoundChannel=SOUND_CHANNEL_MIN;
 }
 
 // Spielt die übergebene ogg/wav/mod-Datei:
-void PlayMusic(char *file){
-  if(!sound||MusicMute)return;
-  music_stream = Mix_LoadMUS(file);
-  if(!music_stream) return;
-  Mix_PlayMusic(music_stream,0);
-  Mix_VolumeMusic(MusicVol);
+void PlayMusic ( char *file )
+{
+	if ( !sound||MusicMute ) return;
+	music_stream = Mix_LoadMUS ( file );
+	if ( !music_stream ) return;
+	Mix_PlayMusic ( music_stream,0 );
+	Mix_VolumeMusic ( MusicVol );
 }
 
 // Setzt das Volume der Musik:
-void SetMusicVol(int vol){
-  if(!sound)return;
-	Mix_VolumeMusic(vol);
+void SetMusicVol ( int vol )
+{
+	if ( !sound ) return;
+	Mix_VolumeMusic ( vol );
 }
 
 // Stoppt die Musik:
-void StopMusic(void){
-  if(!sound||!music_stream)return;
-	Mix_FreeMusic(music_stream);
-  music_stream=NULL;
+void StopMusic ( void )
+{
+	if ( !sound||!music_stream ) return;
+	Mix_FreeMusic ( music_stream );
+	music_stream=NULL;
 }
 
 // Startet die Musik:
-void StartMusic(void){
-  if(!sound)return;
-  PlayMusic((char *)MusicFiles->Items[random(MusicAnz,0)].c_str());
+void StartMusic ( void )
+{
+	if ( !sound ) return;
+	PlayMusic ( ( char * ) MusicFiles->Items[random ( MusicAnz,0 ) ].c_str() );
 }
 
 // Callback, wenn Musik am Ende:
-void MusicFinished(void){
-  if(!sound/*||in_credits*/)return;
-  srand( (unsigned)time( NULL ) );
-  PlayMusic((char *)MusicFiles->Items[random(MusicAnz,0)].c_str());
+void MusicFinished ( void )
+{
+	if ( !sound/*||in_credits*/ ) return;
+	srand ( ( unsigned ) time ( NULL ) );
+	PlayMusic ( ( char * ) MusicFiles->Items[random ( MusicAnz,0 ) ].c_str() );
 }
 
 // Startet einen Loop-Sound:
-int PlayFXLoop(sSOUND *snd){
-  if(!sound)return 0;
-  Mix_HaltChannel(SoundLoopChannel);
-  Mix_PlayChannel(SoundLoopChannel,snd,-1);
-  Mix_Volume(SoundLoopChannel,SoundVol);
-  return SoundLoopChannel;
+int PlayFXLoop ( sSOUND *snd )
+{
+	if ( !sound ) return 0;
+	Mix_HaltChannel ( SoundLoopChannel );
+	Mix_PlayChannel ( SoundLoopChannel,snd,-1 );
+	Mix_Volume ( SoundLoopChannel,SoundVol );
+	return SoundLoopChannel;
 }
 
 // Stoppt einen Loop-Sound:
-void StopFXLoop(int SndStream){
-  if(!sound||SndStream!=SoundLoopChannel)return;
-  Mix_HaltChannel(SoundLoopChannel);
+void StopFXLoop ( int SndStream )
+{
+	if ( !sound||SndStream!=SoundLoopChannel ) return;
+	Mix_HaltChannel ( SoundLoopChannel );
 }
