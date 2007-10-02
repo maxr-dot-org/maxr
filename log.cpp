10c9a96a950013b6190425bb9e1a785686aa7401
Bernd Kosmahl <beko@maxr.org>
2007-10-02 19:32:31 +0000

    fixed mail
    git-svn-id: svn://maxr.org/MAX/MAX@48 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- log.cpp
+++ log.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2007 by Bernd Kosmahl   *
- *   beko@duke.famkos.net   *
+ *   Copyright (C) 2007 by Bernd 'beko' Kosmahl   *
+ *   beko@dmaxthegame.de   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
