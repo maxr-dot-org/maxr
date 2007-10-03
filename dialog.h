0bc2f8af1c07fc6102cb1376fb2bda668f1f6eaf
Albert Ziegenhagel <alzi@maxr.org>
2007-10-03 19:52:15 +0000

    SortFileList-Function removed
    git-svn-id: svn://maxr.org/MAX/MAX@94 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- dialog.h
+++ dialog.h
@@ -8,7 +8,6 @@
 
 void ShowDialog(string text,bool pure,string path,int SaveLoad=-1);
 void ShowDialogList(TList *list,int offset);
-int __fastcall SortFileList(void * Item1, void * Item2);
 bool ShowYesNo(string text);
 int ShowNumberInput(string text);
 void ShowOK(string text,bool pure=false);
