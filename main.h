55639f6f1446bc28cff39741937511e8f6fa4e7b
Albert Ziegenhagel <alzi@maxr.org>
2007-10-05 12:02:18 +0000

    pragma warnings for WIN32 added
    git-svn-id: svn://maxr.org/MAX/MAX@110 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- main.h
+++ main.h
@@ -1,6 +1,18 @@
 //////////////////////////////////////////////////////////////////////////////
 // M.A.X. - main.h
 //////////////////////////////////////////////////////////////////////////////
+#ifdef _WIN32
+#pragma warning(disable:4996)
+#pragma warning(disable:4244)
+#pragma warning(disable:4800)
+#pragma warning(disable:4005)
+
+#pragma warning(disable:4311)
+#pragma warning(disable:4312)
+#pragma warning(disable:4305)
+#pragma warning(disable:4313)
+#pragma warning(disable:4804)
+#endif
 
 #ifndef mainH
 #define mainH
