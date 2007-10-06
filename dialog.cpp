b1372b0a803a98e47bb09218ae7cfb7a31ab2c64
Albert Ziegenhagel <alzi@maxr.org>
2007-10-06 15:01:25 +0000

    main.h tidyed up, prepared for new loadingcode and removed error caused by this in other files
    git-svn-id: svn://maxr.org/MAX/MAX@132 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- dialog.cpp
+++ dialog.cpp
@@ -20,17 +20,17 @@ bool ShowYesNo(string text){
 
   mouse->SetCursor(CHand);
   game->DrawMap(false);
-  SDL_BlitSurface(gfx_hud,NULL,buffer,NULL);
+  SDL_BlitSurface(GraphicsData.gfx_hud,NULL,buffer,NULL);
 
-  if(Alpha){
-    SDL_BlitSurface(gfx_shadow,NULL,buffer,NULL);
+  if(GameSettingsData.Alpha){
+    SDL_BlitSurface(GraphicsData.gfx_shadow,NULL,buffer,NULL);
   }
-  LoadPCXtoSF((char *) Dialog2Path.c_str(),gfx_dialog);
+  LoadPCXtoSF((char *) GraphicsData.Dialog2Path.c_str(),GraphicsData.gfx_dialog);
   dest.x=640/2-300/2;
   dest.y=480/2-231/2;
   dest.w=300;
   dest.h=231;
-  SDL_BlitSurface(gfx_dialog,NULL,buffer,&dest);
+  SDL_BlitSurface(GraphicsData.gfx_dialog,NULL,buffer,&dest);
   PlaceSmallButton("Ja",640/2-300/2+80,480/2-231/2+150,false);
   PlaceSmallButton("Nein",640/2-300/2+80,480/2-231/2+185,false);
   dest.x+=20;dest.w-=40;
@@ -85,7 +85,7 @@ bool ShowYesNo(string text){
     SDL_Delay(1);
   }
 
-  LoadPCXtoSF((char *) DialogPath.c_str(),gfx_dialog);
+  LoadPCXtoSF((char *) GraphicsData.DialogPath.c_str(),GraphicsData.gfx_dialog);
   game->fDrawMap=true;
   return ret;
 }
@@ -99,12 +99,12 @@ int ShowNumberInput(string text){
   bool Cursor = true;
   string stmp;
 
-  LoadPCXtoSF((char *) Dialog3Path.c_str(),gfx_dialog);
+  LoadPCXtoSF((char *) GraphicsData.Dialog3Path.c_str(),GraphicsData.gfx_dialog);
   dest.x=640/2-300/2;
   dest.y=480/2-231/2;
   dest.w=300;
   dest.h=231;
-  SDL_BlitSurface(gfx_dialog,NULL,buffer,&dest);
+  SDL_BlitSurface(GraphicsData.gfx_dialog,NULL,buffer,&dest);
   PlaceSmallButton("OK",640/2-300/2+80,480/2-231/2+185,false);
   dest.x+=20;dest.w-=40;
   dest.y+=20;dest.h-=150;
@@ -134,7 +134,7 @@ int ShowNumberInput(string text){
       dest.h=scr.h=17;
       dest.x=20+170;
       dest.y=167+124;
-      SDL_BlitSurface(gfx_dialog,&scr,buffer,&dest);
+      SDL_BlitSurface(GraphicsData.gfx_dialog,&scr,buffer,&dest);
 	  stmp = InputStr; stmp += "_";
       if(fonts->GetTextLen((char *) stmp.c_str())>=246){
 		  InputStr.erase(InputStr.length()-1, 0);
@@ -167,7 +167,7 @@ int ShowNumberInput(string text){
     SDL_Delay(1);
   }
 
-  LoadPCXtoSF((char *)  DialogPath.c_str(),gfx_dialog);
+  LoadPCXtoSF((char *)  GraphicsData.DialogPath.c_str(),GraphicsData.gfx_dialog);
   value=atoi(InputStr.c_str());
   return value;
 }
@@ -181,20 +181,20 @@ void ShowOK(string text,bool pure){
     dest.x=180;dest.y=18;
     dest.w=dest.h=448;
     SDL_FillRect(buffer,&dest,0);
-    SDL_BlitSurface(gfx_hud,NULL,buffer,NULL);
+    SDL_BlitSurface(GraphicsData.gfx_hud,NULL,buffer,NULL);
     dest.x=15;dest.y=356;
     dest.w=dest.h=112;
     SDL_FillRect(buffer,&dest,0);
-    if(Alpha){
-      SDL_BlitSurface(gfx_shadow,NULL,buffer,NULL);
+    if(GameSettingsData.Alpha){
+      SDL_BlitSurface(GraphicsData.gfx_shadow,NULL,buffer,NULL);
     }
   }
-  LoadPCXtoSF((char *) Dialog2Path.c_str(),gfx_dialog);
+  LoadPCXtoSF((char *) GraphicsData.Dialog2Path.c_str(),GraphicsData.gfx_dialog);
   dest.x=640/2-300/2;
   dest.y=480/2-231/2;
   dest.w=300;
   dest.h=231;
-  SDL_BlitSurface(gfx_dialog,NULL,buffer,&dest);
+  SDL_BlitSurface(GraphicsData.gfx_dialog,NULL,buffer,&dest);
   PlaceSmallButton("OK",640/2-300/2+80,480/2-231/2+185,false);
   dest.x+=20;dest.w-=40;
   dest.y+=20;dest.h-=150;
@@ -236,6 +236,6 @@ void ShowOK(string text,bool pure){
     SDL_Delay(1);
   }
 
-  LoadPCXtoSF((char *) DialogPath.c_str(),gfx_dialog);
+  LoadPCXtoSF((char *) GraphicsData.DialogPath.c_str(),GraphicsData.gfx_dialog);
   if(!pure)game->fDrawMap=true;
 }
