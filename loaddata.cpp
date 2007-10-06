d38ada5f816a2581afdd4e4bd52edb5c1ce12394
Albert Ziegenhagel <alzi@maxr.org>
2007-10-06 16:50:05 +0000

    New files for loading data
    git-svn-id: svn://maxr.org/MAX/MAX@150 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

---
+++ loaddata.cpp
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// M.A.X. Reloaded - main.h
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+// Main-declerations, -classes and structures for the game
+// Contains all global varaibles needed for the game
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "loaddata.h"
+#include "fonts.h"
+
+// LoadData ///////////////////////////////////////////////////////////////////
+// Loads alle relevant files and datas
+int LoadData ( void * )
+{
+	LoadingData=true;
+	for( int i = 0; i < 1000; i++)
+	{
+		SDL_Delay(10);
+	}
+	LoadingData=false;
+	return 1;
+}
+
+// MakeLog ///////////////////////////////////////////////////////////////////
+// Schreibt eine Nachricht auf dem SplashScreen:
+void MakeLog ( char* sztxt,bool ok,int pos )
+{
+	if ( !ok )
+		fonts->OutTextBig ( sztxt,22,152+16*pos,buffer );
+	else
+		fonts->OutTextBig ( "OK",250,152+16*pos,buffer );
+	SDL_BlitSurface ( buffer,NULL,screen,NULL );
+	SDL_UpdateRect ( screen,0,0,0,0 );
+}
