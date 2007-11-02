66108db3a1e86a7ba97bdf9cd54319deb3d3aabd
Bernd Kosmahl <beko@maxr.org>
2007-11-02 21:50:27 +0000

    optimized debug check
    git-svn-id: svn://maxr.org/MAX/MAX@381 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- log.cpp
+++ log.cpp
@@ -37,10 +37,10 @@ static SDL_RWops *logfile = NULL;
 
 bool cLog::open()
 {
-	if (logfile != NULL) 
+	if ( logfile != NULL )
 	{
 		logfile = SDL_RWFromFile ( LOGFILE,"a+t" ); //Reopen log and write at end of file
-	} 
+	}
 	else //Log not yet started (should only happen at game start)
 	{
 		logfile = SDL_RWFromFile ( LOGFILE,"w+t" ); //Start new log and write at beginning of file
@@ -48,34 +48,34 @@ bool cLog::open()
 
 	int blocks; //sanity check - is file readable?
 	char buf[256];
-	blocks=SDL_RWread(logfile,buf,16,256/16);
-	if(blocks<0)
+	blocks=SDL_RWread ( logfile,buf,16,256/16 );
+	if ( blocks<0 )
 	{
-		fprintf(stderr,"(EE): Couldn't open max.log!\n Please check file/directory permissions\n");
+		fprintf ( stderr,"(EE): Couldn't open max.log!\n Please check file/directory permissions\n" );
 
 		if ( logfile != NULL ) return true;
 		else return false;
-	} 
+	}
 	else return true;
-	
+
 }
 
 int cLog::write ( const char *str, int TYPE )
 {
-	return write( std::string( str ) , TYPE ); 
+	return write ( std::string ( str ) , TYPE );
 }
 /*	if (open())
 	{
 		char tmp[264] = "(XX): "; //placeholder
 		if (strlen ( str ) > 264 - 7) //message max is 256chars
-		{ 
+		{
 			std::string str = "(EE): sLog recieved to long log message!";
 			str += TEXT_FILE_LF;
 			str += "(EE): Message had more than 256 chars! That should not happen!";
 			str += TEXT_FILE_LF;
 			return writeMessage( str );
 		}
-		else 
+		else
 		{
 			switch ( TYPE ) //Attach log message type to tmp
 			{
@@ -94,35 +94,30 @@ int cLog::write ( const char *str, int TYPE )
 */
 int cLog::write ( std::string str, int TYPE )
 {
-
-	if (open())
+	if ( TYPE == LOG_TYPE_DEBUG && SettingsData.bDebug || TYPE != LOG_TYPE_DEBUG ) //in case debug is disabled we skip message
 	{
-		switch ( TYPE ) //Attach log message type to tmp
+		if ( open() )
 		{
-			case LOG_TYPE_WARNING : str = str.insert( 0 , WW ); break;
-			case LOG_TYPE_ERROR :   str = str.insert( 0 , EE ); break;
-			case LOG_TYPE_DEBUG :   str = str.insert( 0 , DD ); break;
-			case LOG_TYPE_INFO :    str = str.insert( 0 , II ); break;
-			case LOG_TYPE_MEM :	 str = str.insert( 0 , MM ); break;
-			default :				str = str.insert( 0 , II );
-		}
-		str += TEXT_FILE_LF;
-		if (TYPE == LOG_TYPE_DEBUG && SettingsData.bDebug || TYPE != LOG_TYPE_DEBUG)
-		{
+			switch ( TYPE ) //Attach log message type to tmp
+			{
+				case LOG_TYPE_WARNING : str = str.insert ( 0 , WW ); break;
+				case LOG_TYPE_ERROR :   str = str.insert ( 0 , EE ); break;
+				case LOG_TYPE_DEBUG :   str = str.insert ( 0 , DD ); break;
+				case LOG_TYPE_INFO :    str = str.insert ( 0 , II ); break;
+				case LOG_TYPE_MEM :	 str = str.insert ( 0 , MM ); break;
+				default :				str = str.insert ( 0 , II );
+			}
+			str += TEXT_FILE_LF;
 			return writeMessage ( str ); //add log message itself to tmp and send it for writing
 		}
-		else
-		{
-			close();
-			return 0; 
-		}
+		else return -1;
 	}
-	else return -1;
+	else return 0;
 }
 
-int cLog::write (const char *str )
+int cLog::write ( const char *str )
 {
-	return write ( std::string( str ) , LOG_TYPE_INFO );
+	return write ( std::string ( str ) , LOG_TYPE_INFO );
 //	return write ( str, LOG_TYPE_INFO );
 }
 
@@ -130,12 +125,12 @@ void cLog::mark()
 {
 	std::string str = "==============================(MARK)==============================";
 	str += TEXT_FILE_LF;
-	if(open()) writeMessage( str );
+	if ( open() ) writeMessage ( str );
 }
 
 int cLog::writeMessage ( char *str )
 {
-	return writeMessage( std::string( str ) );
+	return writeMessage ( std::string ( str ) );
 }
 /*	std::size_t wrote;
 	wrote = SDL_RWwrite ( logfile,str,1, (int)strlen ( str ) );
@@ -152,7 +147,7 @@ int cLog::writeMessage ( char *str )
 int cLog::writeMessage ( std::string str )
 {
 	std::size_t wrote;
-	wrote = SDL_RWwrite ( logfile , str.c_str() , 1 , (int) str.length() );
+	wrote = SDL_RWwrite ( logfile , str.c_str() , 1 , ( int ) str.length() );
 
 	if ( wrote<0 ) //sanity check - was file writable?
 	{
