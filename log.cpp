45eea3927463bec8b6db06e6d75ddfdc1b5cd99a
Bernd Kosmahl <beko@maxr.org>
2007-11-02 20:32:28 +0000

    added GPL headers
    git-svn-id: svn://maxr.org/MAX/MAX@377 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- log.cpp
+++ log.cpp
@@ -1,6 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007 by Bernd 'beko' Kosmahl   *
- *   beko@dmaxthegame.de   *
+ *      Mechanized Assault and Exploration Reloaded Projectfile            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
