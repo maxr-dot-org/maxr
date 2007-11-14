9a4bb95527a3d096f77b5317680f738343909b70
Bernd Kosmahl <beko@maxr.org>
2007-11-14 21:27:34 +0000

    preferences dialog now drawed by language for i18n support (not yet perfect but working)
    git-svn-id: svn://maxr.org/MAX/MAX@449 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- prefer.cpp
+++ prefer.cpp
@@ -25,45 +25,157 @@
 // DoPraeferenzen ////////////////////////////////////////////////////////////
 // Zeigt das Präferenzenfenster an:
 void DoPraeferenzen ( void )
-{
-	bool OldMusicMute,OldSoundMute,OldVoiceMute,OldbAutoSave,OldbAnimations,OldbShadows,OldbAlphaEffects,OldbDamageEffects,OldbDamageEffectsVehicles,OldbMakeTracks;
-	bool FertigPressed=false,AbbruchPressed=false,Input=false;
-	int OldiScrollSpeed,OldMusicVol,OldSoundVol,OldVoiceVol;
-	int LastMouseX=0,LastMouseY=0,LastB=0,x,y,b;
+{	
+	bool OldMusicMute, OldSoundMute, OldVoiceMute, OldbAutoSave, OldbAnimations, OldbShadows, OldbAlphaEffects, OldbDamageEffects, OldbDamageEffectsVehicles, OldbMakeTracks;
+	bool FertigPressed = false, AbbruchPressed = false, Input = false;
+	int OldiScrollSpeed, OldMusicVol, OldSoundVol, OldVoiceVol;
+	int LastMouseX = 0, LastMouseY = 0, LastB = 0, x, y, b;
 	string OldName;
 	string stmp;
-	SDL_Rect scr,dest;
+	string sTmp;
+	SDL_Rect scr, dest, rFont;
 	Uint8 *keystate;
-	bool cursor=true;
+	bool cursor = true;
+	
+	//position x of all sliderbars
+	#define BAR_X 140
+	#define BAR_Y 110
+	#define CELLSPACE 20
+	#define CELLSPACE_FONT 20
 
-	OldMusicMute=SettingsData.MusicMute;OldSoundMute=SettingsData.SoundMute;OldVoiceMute=SettingsData.VoiceMute;OldbAutoSave=SettingsData.bAutoSave;OldbAnimations=SettingsData.bAnimations;OldbShadows=SettingsData.bShadows;OldbAlphaEffects=SettingsData.bAlphaEffects;
-	OldiScrollSpeed=SettingsData.iScrollSpeed;OldMusicVol=SettingsData.MusicVol;OldSoundVol=SettingsData.SoundVol;OldVoiceVol=SettingsData.VoiceVol;
-	OldName=game->ActivePlayer->name;
-	OldbDamageEffects=SettingsData.bDamageEffects;OldbDamageEffectsVehicles=SettingsData.bDamageEffectsVehicles;OldbMakeTracks=SettingsData.bMakeTracks;
+	OldMusicMute = SettingsData.MusicMute;
+	OldSoundMute = SettingsData.SoundMute;
+	OldVoiceMute = SettingsData.VoiceMute;
+	OldbAutoSave = SettingsData.bAutoSave;
+	OldbAnimations = SettingsData.bAnimations;
+	OldbShadows = SettingsData.bShadows;
+	OldbAlphaEffects = SettingsData.bAlphaEffects;
+	OldiScrollSpeed = SettingsData.iScrollSpeed;
+	OldMusicVol = SettingsData.MusicVol;
+	OldSoundVol = SettingsData.SoundVol;
+	OldVoiceVol = SettingsData.VoiceVol;
+	OldName = game->ActivePlayer->name;
+	OldbDamageEffects = SettingsData.bDamageEffects;
+	OldbDamageEffectsVehicles = SettingsData.bDamageEffectsVehicles;
+	OldbMakeTracks = SettingsData.bMakeTracks;
+	
+	dest.x = 120;
+	dest.y = 29;
+	dest.w = GraphicsData.gfx_praefer->w;
+	dest.h = GraphicsData.gfx_praefer->h;
+	SDL_BlitSurface ( GraphicsData.gfx_hud, NULL, buffer, NULL );
+	
+	if ( SettingsData.bAlphaEffects )
+		SDL_BlitSurface ( GraphicsData.gfx_shadow, NULL, buffer, NULL );
+	SDL_BlitSurface ( GraphicsData.gfx_praefer, NULL, buffer, &dest );
 
-	dest.x=120;
-	dest.y=29;
-	dest.w=GraphicsData.gfx_praefer->w;
-	dest.h=GraphicsData.gfx_praefer->h;
-	SDL_BlitSurface ( GraphicsData.gfx_hud,NULL,buffer,NULL );
-	if ( SettingsData.bAlphaEffects ) SDL_BlitSurface ( GraphicsData.gfx_shadow,NULL,buffer,NULL );
-	SDL_BlitSurface ( GraphicsData.gfx_praefer,NULL,buffer,&dest );
+				scr.x=68;
+				scr.y=172;
+				dest.w=scr.w=63;
+				dest.h=scr.h=24;
+				dest.x=215+120;
+				dest.y=383+29;
+				SDL_BlitSurface ( GraphicsData.gfx_hud_stuff,&scr,buffer,&dest ); //finnished button
+			
+				scr.x=0;
+				scr.y=190;
+				dest.w=scr.w=63;
+				dest.h=scr.h=24;
+				dest.x=125+120;
+				dest.y=383+29;
+				SDL_BlitSurface ( GraphicsData.gfx_hud_stuff,&scr,buffer,&dest );
+				//exit button
 
-	ShowBar ( 74+120,81+29,SettingsData.MusicVol*2 );
-	ShowBar ( 84+120,101+29,SettingsData.SoundVol*2 );
-	ShowBar ( 80+120,121+29,SettingsData.VoiceVol*2 );
-	ShowBar ( 121+120,261+29,SettingsData.iScrollSpeed*5 );
+	rFont.x = 120 + 160; //TOFIX: Text not centered
+	rFont.y = 29 + 15;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Preferences" ).c_str(),rFont.x,rFont.y,buffer);
+
+
+	//BEGIN BLOCK SOUND
+	//Headline
+	sTmp = lngPack.Translate ( "Text~Game_Settings~Title_Volume" ) + ":";
+	rFont.x = 145;
+	rFont.y = 85;
+	rFont.w = fonts->GetTextLen(sTmp.c_str());
+	fonts->OutText(sTmp.c_str(),rFont.x,rFont.y,buffer);
+
+	//Music
+	rFont.x = 145; 	rFont.w = 100;
+	rFont.y = 105;
+	rFont.h = CELLSPACE_FONT;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Music" ).c_str(),rFont.x,rFont.y,buffer);
+	ShowBar ( BAR_X+120,BAR_Y,SettingsData.MusicVol*2 );
 	SetButton ( 210+120,73+29,SettingsData.MusicMute );
+	rFont.x = 355; 	rFont.w = 140;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Disable" ).c_str(),rFont.x,rFont.y,buffer);
+		
+	//Effectsound
+	rFont.x = 145; 	rFont.w = 100;
+	rFont.y += CELLSPACE_FONT;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Effects" ).c_str(),rFont.x,rFont.y,buffer);
+	ShowBar ( BAR_X+120,BAR_Y+CELLSPACE,SettingsData.SoundVol*2 );
 	SetButton ( 210+120,93+29,SettingsData.SoundMute );
+	rFont.x = 355; 	rFont.w = 140;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Disable" ).c_str(),rFont.x,rFont.y,buffer);
+
+	//Voices
+	rFont.x = 145; 	rFont.w = 100;
+	rFont.y += CELLSPACE_FONT;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Voices" ).c_str(),rFont.x,rFont.y,buffer);
+	ShowBar ( BAR_X+120,BAR_Y+CELLSPACE*2,SettingsData.VoiceVol*2 );
 	SetButton ( 210+120,113+29,SettingsData.VoiceMute );
-	SetButton ( 25+120,133+29,SettingsData.bAutoSave );
-	SetButton ( 25+120,193+29,SettingsData.bAnimations );
-	SetButton ( 25+120,213+29,SettingsData.bShadows );
-	SetButton ( 25+120,233+29,SettingsData.bAlphaEffects );
-	SetButton ( 210+120,193+29,SettingsData.bDamageEffects );
-	SetButton ( 210+120,213+29,SettingsData.bDamageEffectsVehicles );
-	SetButton ( 210+120,233+29,SettingsData.bMakeTracks );
+	rFont.x = 355; 	rFont.w = 140;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Disable" ).c_str(),rFont.x,rFont.y,buffer);
+	//END BLOCK SOUND
+	//BEGIN BLOCK PLAYERNAME	
+	
+	rFont.x = 145; 	rFont.w = 100;
+	rFont.y = 158+29;
+	fonts->OutText(lngPack.Translate( "Text~Game_Start~Title_Player_Name" ).c_str(),rFont.x,rFont.y,buffer);
 	fonts->OutText ( ( char * ) game->ActivePlayer->name.c_str(),122+120,158+29,buffer );
+
+	//END BLOCK PLAYERNAME
+
+	rFont.x = 145+25; rFont.w = 100;
+	rFont.y = 193+33;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Animation" ).c_str(),rFont.x,rFont.y,buffer);
+	SetButton ( 25+120,193+29,SettingsData.bAnimations );
+	
+	rFont.x = 145+25; rFont.w = 100;
+	rFont.y = 213+33;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Shadows" ).c_str(),rFont.x,rFont.y,buffer);
+	SetButton ( 25+120,213+29,SettingsData.bShadows );
+	
+	rFont.x = 145+25; rFont.w = 100;
+	rFont.y = 233+33;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Alphaeffects" ).c_str(),rFont.x,rFont.y,buffer);
+	SetButton ( 25+120,233+29,SettingsData.bAlphaEffects );
+	
+	rFont.x = 145+210; rFont.w = 100;
+	rFont.y = 193+33;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_ShowDamage" ).c_str(),rFont.x,rFont.y,buffer);
+	SetButton ( 210+120,193+29,SettingsData.bDamageEffects );
+	
+	rFont.x = 145+210; rFont.w = 100;
+	rFont.y = 213+33;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_ShowDamageVehicle" ).c_str(),rFont.x,rFont.y,buffer);
+	SetButton ( 210+120,213+29,SettingsData.bDamageEffectsVehicles );
+	
+	rFont.x = 145+210; rFont.w = 100;
+	rFont.y = 233+33;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Tracks" ).c_str(),rFont.x,rFont.y,buffer);
+	SetButton ( 210+120,233+29,SettingsData.bMakeTracks );
+	
+	rFont.x = 145; 	rFont.w = 100;
+	rFont.y = 261+25;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Scrollspeed" ).c_str(),rFont.x,rFont.y,buffer);
+	ShowBar ( BAR_X+120,261+29,SettingsData.iScrollSpeed*5 );
+	
+	rFont.x = 145+25; rFont.w = 100;
+	rFont.y = 290+33;
+	fonts->OutText(lngPack.Translate( "Text~Game_Settings~Title_Autosave" ).c_str(),rFont.x,rFont.y,buffer);
+	SetButton ( 25+120,290+29,SettingsData.bAutoSave );
+	
 	SHOW_SCREEN
 
 	mouse->GetBack ( buffer );
@@ -126,36 +238,36 @@ void DoPraeferenzen ( void )
 			mouse->draw ( true,screen );
 		}
 		if ( b )
-		{
-			if ( x>=74+120&&x<74+120+57&&y>=81+29-7&&y<=81+29+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
+		{		//firstbar
+			if ( x>=BAR_X+120&&x<BAR_X+120+57&&y>=81+29-7&&y<=81+29+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
 			{
-				SettingsData.MusicVol= ( x- ( 74+120 ) ) * (int)( 128.0/57 );
+				SettingsData.MusicVol= ( x- ( BAR_X+120 ) ) * (int)( 128.0/57 );
 				if ( SettingsData.MusicVol>=125 ) SettingsData.MusicVol=128;
-				ShowBar ( 74+120,81+29,SettingsData.MusicVol*2 );
+				ShowBar ( BAR_X+120,81+29,SettingsData.MusicVol*2 );
 				SHOW_SCREEN
 				mouse->draw ( false,screen );
 				SetMusicVol ( SettingsData.MusicVol );
 			}
-			else if ( x>=84+120&&x<84+120+57&&y>=101+29-7&&y<=101+29+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
+			else if ( x>=BAR_X+120&&x<BAR_X+120+57&&y>=BAR_Y+CELLSPACE-7&&y<=BAR_Y+CELLSPACE+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
 			{
-				SettingsData.SoundVol= ( x- ( 84+120 ) ) * (int)( 128.0/57 );
+				SettingsData.SoundVol= ( x- ( BAR_X+120 ) ) * (int)( 128.0/57 );
 				if ( SettingsData.SoundVol>=125 ) SettingsData.SoundVol=128;
-				ShowBar ( 84+120,101+29,SettingsData.SoundVol*2 );
+				ShowBar ( BAR_X+120,BAR_Y+CELLSPACE,SettingsData.SoundVol*2 );
 				SHOW_SCREEN
 				mouse->draw ( false,screen );
 			}
-			else if ( x>=80+120&&x<80+120+57&&y>=121+29-7&&y<=121+29+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
+			else if ( x>=BAR_X+120&&x<BAR_X+120+57&&y>=BAR_Y+CELLSPACE*2-7&&y<=BAR_Y+CELLSPACE*2+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
 			{
-				SettingsData.VoiceVol= ( x- ( 80+120 ) ) * (int)( 128.0/57 );
+				SettingsData.VoiceVol= ( x- ( BAR_X+120 ) ) * (int)( 128.0/57 );
 				if ( SettingsData.VoiceVol>=125 ) SettingsData.VoiceVol=128;
-				ShowBar ( 80+120,121+29,SettingsData.VoiceVol*2 );
+				ShowBar ( BAR_X+120,BAR_Y+CELLSPACE*2,SettingsData.VoiceVol*2 );
 				SHOW_SCREEN
 				mouse->draw ( false,screen );
 			}
-			else if ( x>=121+120&&x<121+120+57&&y>=261+29-7&&y<=261+29+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
+			else if ( x>=BAR_X+120&&x<BAR_X+120+57&&y>=261+29-7&&y<=261+29+10&& ( x!=LastMouseX||y!=LastMouseY||!LastB ) )
 			{
-				SettingsData.iScrollSpeed= ( int ) ( ( x- ( 121+120 ) ) * ( 255.0/57 ) ) /5;
-				ShowBar ( 121+120,261+29,SettingsData.iScrollSpeed*5 );
+				SettingsData.iScrollSpeed= ( int ) ( ( x- ( BAR_X+120 ) ) * ( 255.0/57 ) ) /5;
+				ShowBar ( BAR_X+120,261+29,SettingsData.iScrollSpeed*5 );
 				SHOW_SCREEN
 				mouse->draw ( false,screen );
 			}
@@ -190,10 +302,10 @@ void DoPraeferenzen ( void )
 					SHOW_SCREEN
 					mouse->draw ( false,screen );
 				}
-				else if ( x>=25+120&&x<25+120+18&&y>=133+29&&y<133+29+17 )
+				else if ( x>=25+120&&x<25+120+18&&y>=290+29&&y<290+29+17 )
 				{
 					SettingsData.bAutoSave=!SettingsData.bAutoSave;
-					SetButton ( 25+120,133+29,SettingsData.bAutoSave );
+					SetButton ( 25+120,290+29,SettingsData.bAutoSave );
 					SHOW_SCREEN
 					mouse->draw ( false,screen );
 				}
@@ -312,10 +424,21 @@ void DoPraeferenzen ( void )
 			}
 			else if ( !b&&LastB )
 			{
-				SettingsData.MusicMute=OldMusicMute;SettingsData.SoundMute=OldSoundMute;SettingsData.VoiceMute=OldVoiceMute;SettingsData.bAutoSave=OldbAutoSave;SettingsData.bAnimations=OldbAnimations;SettingsData.bShadows=OldbShadows;SettingsData.bAlphaEffects=OldbAlphaEffects;
-				SettingsData.iScrollSpeed=OldiScrollSpeed;SettingsData.MusicVol=OldMusicVol;SettingsData.SoundVol=OldSoundVol;SettingsData.VoiceVol=OldVoiceVol;
-				SettingsData.bDamageEffects=OldbDamageEffects;SettingsData.bDamageEffectsVehicles=OldbDamageEffectsVehicles;SettingsData.bMakeTracks=OldbMakeTracks;
-				game->ActivePlayer->name=OldName;
+				SettingsData.MusicMute = OldMusicMute;
+				SettingsData.SoundMute = OldSoundMute;
+				SettingsData.VoiceMute = OldVoiceMute;
+				SettingsData.bAutoSave = OldbAutoSave;
+				SettingsData.bAnimations = OldbAnimations;
+				SettingsData.bShadows = OldbShadows;
+				SettingsData.bAlphaEffects = OldbAlphaEffects;
+				SettingsData.iScrollSpeed = OldiScrollSpeed;
+				SettingsData.MusicVol = OldMusicVol;
+				SettingsData.SoundVol = OldSoundVol;
+				SettingsData.VoiceVol = OldVoiceVol;
+				SettingsData.bDamageEffects = OldbDamageEffects;
+				SettingsData.bDamageEffectsVehicles = OldbDamageEffectsVehicles;
+				SettingsData.bMakeTracks = OldbMakeTracks;
+				game->ActivePlayer->name = OldName;
 				SetMusicVol ( SettingsData.MusicVol );
 				return;
 			}
@@ -341,22 +464,46 @@ void DoPraeferenzen ( void )
 	}
 }
 
+ //FIXME: offset method only works on fixed resolution 640x460. 
 void ShowBar ( int offx,int offy,int value )
 {
-	SDL_Rect scr,dest;
-	scr.x=offx-120-6;
-	scr.y=offy-29-7;
-	dest.w=scr.w=57+14;
-	dest.h=scr.h=17;
-	dest.x=offx-6;
-	dest.y=offy-7;
-	SDL_BlitSurface ( GraphicsData.gfx_praefer,&scr,buffer,&dest );
+	SDL_Rect scr, dest;
+	#define SLIDER_W 14
+	#define SLIDER_H 17
+	/*Offset to read clean background from +/- 7 to 
+	*overdraw slider because slider is 14 fat and can
+	*show half over the ends of the sliderbar*/
+	scr.x = offx - 120 - SLIDER_W / 2; //scr.x & scr.y = topleft
+	scr.y = offy - 29 ;
+	scr.w = 57 + SLIDER_W / 2;
+	scr.h = SLIDER_H;
+	
+	dest.x = offx - 6;
+	dest.y = offy - 7;	
+	dest.w = scr.w + 6;
+	dest.h = SLIDER_H;
 
-	scr.x=412;
+	//SDL_FillRect(GraphicsData.gfx_praefer,&scr,0x00C000);
+	SDL_BlitSurface ( GraphicsData.gfx_praefer,&scr,buffer,&dest ); //dist
+
+	//SDL_BlitSurface ( GraphicsData.gfx_praefer,&dest,buffer,&dest );
+	
+	scr.x=334;
+	scr.y=82;
+	scr.w=58;
+	scr.h=3;	
+	dest.y += 7;
+	SDL_BlitSurface ( GraphicsData.gfx_hud_stuff,&scr,buffer,&dest );
+
+	scr.x=412; //get slider on hud_stuff.pcx
 	scr.y=46;
-	dest.w=scr.w=14;
+	scr.w=SLIDER_W;
+	scr.h=SLIDER_H;
+	
+	dest.w=scr.w;
+	dest.h=scr.h;
 	dest.x=offx-6+ ( int ) ( ( 57/255.0 ) *value );
-	dest.y=offy-7;
+	dest.y=offy-SLIDER_W / 2;
 	SDL_BlitSurface ( GraphicsData.gfx_hud_stuff,&scr,buffer,&dest );
 }
 
