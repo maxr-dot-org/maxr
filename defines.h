cb19bb270dbc233ee24fde8763290ffe543e913b
Bernd Kosmahl <beko@maxr.org>
2008-01-02 19:28:14 +0000

    added GPL-header moved main.cpp to resinstaller.cpp added missing newlines

--- defines.h
+++ defines.h
@@ -1,12 +1,28 @@
-/*==============================================================================*
-|																				|
-|									defines.h									|
-*===============================================================================*/
+/***************************************************************************
+ *              Resinstaller - installs missing GFX for MAXR               *
+ *              This file is part of the resinstaller project              *
+ *   Copyright (C) 2007, 2008 Eiko Oltmanns                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #ifndef DefinesH
 #define DefinesH
 
-#ifdef __main__
+#ifdef __resinstaller__
 #define EX
 #define ZERO =0
 #define ONE =1
@@ -27,3 +43,5 @@ Effektcolors:  11,14,21,22,	...
 most likely, all colors < 64 have a special meaning
 
 */
+
+
--- resinstaller.h
+++ resinstaller.h
@@ -19,73 +19,21 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
  
-#ifndef ConverterH
-#define ConverterH
-#include <SDL.h>
+#ifndef ResinstallerH
+#define ResinstallerH
 #include "defines.h"
-#include "resinstaller.h"
+#include <iostream>
 
-struct sPixel
-{
-	unsigned char Blue, Green, Red;
-};
+using namespace std;
 
-struct cImageData
-{
-	short sWidth;
-	short sHeight;
-	short sHotX;
-	short sHotY;
-	short sUHotX;
-	short sUHotY;
+EX string sMAXPath;
+EX string sPalettePath;
+EX string sOutputPath;
+EX FILE *res ZERO;
 
-	cImageData *Images;
+EX unsigned char *orig_palette;
+EX long int lPosBegin;
+EX long int lEndOfFile;
 
-	unsigned char *data;
-	unsigned char *alpha;
 
-	SDL_Surface* surface;
-};
-
-class cImage
-{
-	bool bDecoded;
-	short sWidth;
-	short sHeight;
-	cImageData *Images;
-
-	int iImageCount;
-
-	short sHotX;
-	short sHotY;
-
-	bool decodeSimpleImage();
-	bool decodeMultiShadow();
-	bool decodeMultiImage();
-	bool decodeBigImage();
-
-public:
-	
-	cImage();
-	~cImage();
-
-	char name[9];
-	long lPos;
-	long lLenght;
-
-	sPixel *palette;
-
-	void decodeFile();
-	void resampleFile();
-	void saveFile();
-	SDL_Surface* getSurface(int imageNr = 0);
-};
-
-SDL_Surface* getImage(string file_name, int imageNr = 0);
-void removePlayerColor( SDL_Surface *surface);
-int saveAllFiles();
-int copyFileFromRes ( string src, string dst, int number = 0 );
-int copyFileFromRes_rpc(string src, string dst, int number = 0 );
-int copyImageFromFLC(string fileName, string dst);
-
-#endif // ConvertH
+#endif // ResinstallerH
