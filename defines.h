2830842b03cf89307fd24e724a14112abd2f33dd
Eiko Oltmanns <eiko@maxr.org>
2008-06-08 10:52:48 +0000

    applied patch max_remove_unused_macro_ONE by tron
    git-svn-id: svn://maxr.org/MAX/trunk@1088 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- defines.h
+++ defines.h
@@ -22,11 +22,9 @@
 #ifdef __main__
 #define EX
 #define ZERO =0
-#define ONE =1
 #else
 #define EX extern
 #define ZERO
-#define ONE
 #endif
 
 #define SHOW_SCREEN SDL_BlitSurface(buffer,NULL,screen,NULL);if(SettingsData.bWindowMode)SDL_UpdateRect(screen,0,0,0,0);else{SDL_Flip(screen);}
