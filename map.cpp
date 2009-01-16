4f55431d24af0a30f41148cebf50bbed3a2fd099
Albert Ziegenhagel <alzi@maxr.org>
2009-01-16 18:34:29 +0000

    fixed that units can drive on waterplattforms with explode_mines on it. (fixes #247)
    git-svn-id: svn://maxr.org/MAX/trunk@1851 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- map.cpp
+++ map.cpp
@@ -1146,8 +1146,7 @@ bool cMap::possiblePlaceVehicle( const sUnitData& vehicleData, int offset, const
 
 				//vehicle can drive on water, if there is a bridge, platform or road
 				if ( !building ) return false;
-				//FIXME: what about e.g. an explosive mine in a waterplatform?
-				if ( !( building->data.is_road || building->data.is_bridge || building->data.is_platform )) return false;
+				if ( !( building->data.is_road || building->data.is_bridge || building->data.is_platform || building->data.is_expl_mine )) return false;
 			}
 			if ( player && !player->ScanMap[offset] ) return true;			
 
