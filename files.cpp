e716efd299eedf27ade7843955a6ee9772909f4c
Joris Dauphin <joda@maxr.org>
2013-02-05 10:26:03 +0000

    Should fix compilation on big-endian machine. (not tested in such machine).
    git-svn-id: svn://maxr.org/MAX/trunk@3084 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- files.cpp
+++ files.cpp
@@ -176,15 +176,15 @@ std::string getUserScreenshotsDir()
 
 Uint32 calcCheckSum (Uint32 data, Uint32 checksum)
 {
-	return calcCheckSum ( (char*) &SDL_SwapLE32 (data), 4, checksum);
+	data = SDL_SwapLE32 (data);// The calculation must be endian safe.
+	return calcCheckSum ( reinterpret_cast<char*> (&data), 4, checksum);
 }
 
 Uint32 calcCheckSum (const char* data, size_t dataSize, Uint32 checksum)
 {
-	// NOTE: The calculation must be endian safe.
 	for (const char* i = data; i != data + dataSize; ++i)
 	{
-		checksum  = checksum << 1 | checksum >> 31; // Rotate left by one.
+		checksum = checksum << 1 | checksum >> 31; // Rotate left by one.
 		checksum += *i;
 	}
 	return checksum;
