50a2d7dc71e6c3d74c61ced413f482f84eccf287
Albert Ziegenhagel <alzi@maxr.org>
2014-02-06 13:38:29 +0000

    Fixed minimal Visual C++ version
    git-svn-id: svn://maxr.org/MAX/trunk@3495 c3bd5e88-ca0b-48cf-b972-da51c77c4c1b

--- config/compiler/vcpp.h
+++ config/compiler/vcpp.h
@@ -17,7 +17,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#if _MSC_VER < 1700
+#if _MSC_VER < 1600
 #   ifndef MAXR_NO_ASSERT_CONFIG
 #       error "Visual C++ version < 16.0 (Visual Studio < 2010) will not be able to compile this code because of missing C++11 features."
 #   endif
