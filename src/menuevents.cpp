b5be34c829c2ee9cbefef62c367c7d81d22d2e94
joris <joda@maxr.org>
2015-01-03 17:18:22 +0100

    Remove dynamic allocations in menuevents.cpp for cNetMessage

--- src/menuevents.cpp
+++ src/menuevents.cpp
@@ -33,165 +33,164 @@ namespace
 {
 
 //------------------------------------------------------------------------------
-void sendMessage (cTCP& network, cNetMessage* message, const cPlayerBasicData* player = nullptr, int fromPlayerNr = -1)
+void sendMessage (cTCP& network, cNetMessage& message, const cPlayerBasicData* player = nullptr, int fromPlayerNr = -1)
 {
 	// Attention: The playernumber will only be the real player number
 	// when it is passed to this function explicitly.
 	// Otherwise it is only -1!
-	message->iPlayerNr = fromPlayerNr;
+	message.iPlayerNr = fromPlayerNr;
 
-	if (player == nullptr) network.send (message->iLength, message->serialize());
-	else network.sendTo (player->getSocketIndex(), message->iLength, message->serialize());
+	if (player == nullptr) network.send (message.iLength, message.serialize());
+	else network.sendTo (player->getSocketIndex(), message.iLength, message.serialize());
 
-	Log.write ("Menu: --> " + message->getTypeAsString() + ", Hexdump: " + message->getHexDump(), cLog::eLOG_TYPE_NET_DEBUG);
-	delete message;
+	Log.write ("Menu: --> " + message.getTypeAsString() + ", Hexdump: " + message.getHexDump(), cLog::eLOG_TYPE_NET_DEBUG);
 }
 
 }
 
 void sendMenuChatMessage (cTCP& network, const string& chatMsg, const cPlayerBasicData* player, int fromPlayerNr, bool translationText)
 {
-	cNetMessage* message = new cNetMessage (MU_MSG_CHAT);
-	message->pushString (chatMsg);
-	message->pushBool (translationText);
+	cNetMessage message (MU_MSG_CHAT);
+	message.pushString (chatMsg);
+	message.pushBool (translationText);
 	sendMessage (network, message, player, fromPlayerNr);
 }
 
 void sendRequestIdentification (cTCP& network, const cPlayerBasicData& player)
 {
-	cNetMessage* message = new cNetMessage (MU_MSG_REQ_IDENTIFIKATION);
-	message->pushInt16 (player.getNr());
-	message->pushString (string (PACKAGE_VERSION) + " " + PACKAGE_REV);
+	cNetMessage message (MU_MSG_REQ_IDENTIFIKATION);
+	message.pushInt16 (player.getNr());
+	message.pushString (string (PACKAGE_VERSION) + " " + PACKAGE_REV);
 	sendMessage (network, message, &player);
 }
 
 void sendPlayerNumber (cTCP& network, const cPlayerBasicData& player)
 {
-	cNetMessage* message = new cNetMessage (MU_MSG_PLAYER_NUMBER);
-	message->pushInt16 (player.getNr());
+	cNetMessage message (MU_MSG_PLAYER_NUMBER);
+	message.pushInt16 (player.getNr());
 	sendMessage (network, message, &player);
 }
 
 void sendPlayerList (cTCP& network, const std::vector<std::shared_ptr<cPlayerBasicData>>& players)
 {
-	cNetMessage* message = new cNetMessage (MU_MSG_PLAYERLIST);
+	cNetMessage message (MU_MSG_PLAYERLIST);
 
 	for (int i = (int) players.size() - 1; i >= 0; i--)
 	{
 		const cPlayerBasicData& player = *players[i];
-		message->pushInt16 (player.getNr());
-		message->pushBool (player.isReady());
-		message->pushColor (player.getColor().getColor());
-		message->pushString (player.getName());
+		message.pushInt16 (player.getNr());
+		message.pushBool (player.isReady());
+		message.pushColor (player.getColor().getColor());
+		message.pushString (player.getName());
 	}
-	message->pushInt16 ((int) players.size());
+	message.pushInt16 ((int) players.size());
 	sendMessage (network, message);
 }
 
 void sendGameData (cTCP& network, const cStaticMap* map, const cGameSettings* settings, const std::vector<cPlayerBasicData>& savePlayers, const std::string& saveGameName, const cPlayerBasicData* player)
 {
-	cNetMessage* message = new cNetMessage (MU_MSG_OPTINS);
+	cNetMessage message (MU_MSG_OPTINS);
 
-	message->pushString (saveGameName);
+	message.pushString (saveGameName);
 	for (auto player : savePlayers)
 	{
-		message->pushInt32 (player.getNr());
-		message->pushString (player.getName());
+		message.pushInt32 (player.getNr());
+		message.pushString (player.getName());
 	}
-	message->pushInt32 (savePlayers.size());
+	message.pushInt32 (savePlayers.size());
 
 	if (map)
 	{
 		const std::string mapName = map->getName();
-		message->pushInt32 (MapDownload::calculateCheckSum (mapName));
-		message->pushString (mapName);
+		message.pushInt32 (MapDownload::calculateCheckSum (mapName));
+		message.pushString (mapName);
 	}
-	message->pushBool (map != nullptr);
+	message.pushBool (map != nullptr);
 
 	if (settings)
 	{
-		settings->pushInto (*message);
+		settings->pushInto (message);
 	}
-	message->pushBool (settings != nullptr);
+	message.pushBool (settings != nullptr);
 
 	sendMessage (network, message, player);
 }
 
 void sendIdentification (cTCP& network, const cPlayerBasicData& player)
 {
-	cNetMessage* message = new cNetMessage (MU_MSG_IDENTIFIKATION);
-	message->pushString (string (PACKAGE_VERSION) + " " + PACKAGE_REV);
-	message->pushBool (player.isReady());
-	message->pushString (player.getName());
-	message->pushColor (player.getColor().getColor());
-	message->pushInt16 (player.getNr());
+	cNetMessage message (MU_MSG_IDENTIFIKATION);
+	message.pushString (string (PACKAGE_VERSION) + " " + PACKAGE_REV);
+	message.pushBool (player.isReady());
+	message.pushString (player.getName());
+	message.pushColor (player.getColor().getColor());
+	message.pushInt16 (player.getNr());
 	sendMessage (network, message);
 }
 
 void sendGameIdentification (cTCP& network, const cPlayerBasicData& player, int socket)
 {
-	cNetMessage* message = new cNetMessage (GAME_EV_IDENTIFICATION);
-	message->pushInt16 (socket);
-	message->pushString (player.getName());
+	cNetMessage message (GAME_EV_IDENTIFICATION);
+	message.pushInt16 (socket);
+	message.pushString (player.getName());
 	sendMessage (network, message);
 }
 
 void sendRequestMap (cTCP& network, const string& mapName, int playerNr)
 {
-	cNetMessage* msg = new cNetMessage (MU_MSG_REQUEST_MAP);
-	msg->pushString (mapName);
-	msg->pushInt16 (playerNr);
-	sendMessage (network, msg);
+	cNetMessage message (MU_MSG_REQUEST_MAP);
+	message.pushString (mapName);
+	message.pushInt16 (playerNr);
+	sendMessage (network, message);
 }
 
 //------------------------------------------------------------------------------
 void sendGo (cTCP& network)
 {
-	cNetMessage* msg = new cNetMessage (MU_MSG_GO);
-	sendMessage (network, msg);
+	cNetMessage message (MU_MSG_GO);
+	sendMessage (network, message);
 }
 
 //------------------------------------------------------------------------------
 void sendLandingState (cTCP& network, eLandingPositionState state, const cPlayerBasicData& player)
 {
-	cNetMessage* msg = new cNetMessage (MU_MSG_LANDING_STATE);
-	msg->pushInt32 (toUnderlyingType (state));
+	cNetMessage message (MU_MSG_LANDING_STATE);
+	message.pushInt32 (toUnderlyingType (state));
 
-	sendMessage (network, msg, &player);
+	sendMessage (network, message, &player);
 }
 
 //------------------------------------------------------------------------------
 void sendAllLanded (cTCP& network)
 {
-	cNetMessage* msg = new cNetMessage (MU_MSG_ALL_LANDED);
-	sendMessage (network, msg);
+	cNetMessage message (MU_MSG_ALL_LANDED);
+	sendMessage (network, message);
 }
 
 //------------------------------------------------------------------------------
 void sendLandingPosition (cTCP& network, const cPosition& position, const cPlayerBasicData& player)
 {
-	cNetMessage* msg = new cNetMessage (MU_MSG_LANDING_POSITION);
-	msg->pushPosition (position);
-	msg->pushInt32 (player.getNr());
+	cNetMessage message (MU_MSG_LANDING_POSITION);
+	message.pushPosition (position);
+	message.pushInt32 (player.getNr());
 
-	sendMessage (network, msg);
+	sendMessage (network, message);
 }
 
 //------------------------------------------------------------------------------
 void sendInLandingPositionSelectionStatus (cTCP& network, const cPlayerBasicData& player, bool isIn, const cPlayerBasicData* receiver)
 {
-	cNetMessage* msg = new cNetMessage (MU_MSG_IN_LANDING_POSITION_SELECTION_STATUS);
-	msg->pushInt32 (player.getNr());
-	msg->pushBool (isIn);
+	cNetMessage message (MU_MSG_IN_LANDING_POSITION_SELECTION_STATUS);
+	message.pushInt32 (player.getNr());
+	message.pushBool (isIn);
 
-	sendMessage (network, msg, receiver);
+	sendMessage (network, message, receiver);
 }
 
 //------------------------------------------------------------------------------
 void sendPlayerHasSelectedLandingPosition (cTCP& network, const cPlayerBasicData& player, const cPlayerBasicData* receiver)
 {
-	cNetMessage* msg = new cNetMessage (MU_MSG_PLAYER_HAS_SELECTED_LANDING_POSITION);
-	msg->pushInt32 (player.getNr());
+	cNetMessage message (MU_MSG_PLAYER_HAS_SELECTED_LANDING_POSITION);
+	message.pushInt32 (player.getNr());
 
-	sendMessage (network, msg, receiver);
+	sendMessage (network, message, receiver);
 }
