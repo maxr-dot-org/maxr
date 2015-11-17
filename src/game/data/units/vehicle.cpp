maxr-dot-org/maxr

commit 91abd47f3470bd7dffafb9db1b2cd58b6356f28e

Fixed drawing uni images in reports menu

src/game/data/units/vehicle.cpp
@@ -115,9 +115,14 @@ cVehicle::~cVehicle()
 
 void cVehicle::drawOverlayAnimation (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int frameNr, int alpha) const
 {
+	drawOverlayAnimation (surface, dest, zoomFactor, data, *uiData, frameNr, alpha);
+}
+
+/*static*/ void cVehicle::drawOverlayAnimation (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, const sUnitData& data, const sVehicleUIData& uiData, int frameNr, int alpha)
+{
 	if (data.hasOverlay == false || cSettings::getInstance().isAnimations() == false) return;
 
-	const Uint16 size = (Uint16) (uiData->overlay_org->h * zoomFactor);
+	const Uint16 size = (Uint16) (uiData.overlay_org->h * zoomFactor);
 	SDL_Rect src = {Sint16 (frameNr * size), 0, size, size};
 
 	SDL_Rect tmp = dest;
@@ -125,8 +130,8 @@ void cVehicle::drawOverlayAnimation (SDL_Surface* surface, const SDL_Rect& dest,
 	tmp.x += offset;
 	tmp.y += offset;
 
-    SDL_SetSurfaceAlphaMod (uiData->overlay.get (), alpha);
-    blitWithPreScale (uiData->overlay_org.get (), uiData->overlay.get (), &src, surface, &tmp, zoomFactor);
+	SDL_SetSurfaceAlphaMod (uiData.overlay.get (), alpha);
+	blitWithPreScale (uiData.overlay_org.get (), uiData.overlay.get (), &src, surface, &tmp, zoomFactor);
 }
 
 void cVehicle::drawOverlayAnimation (unsigned long long animationTime, SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor) const
@@ -139,7 +144,7 @@ void cVehicle::drawOverlayAnimation (unsigned long long animationTime, SDL_Surfa
 		frameNr = animationTime % (uiData->overlay_org->w / uiData->overlay_org->h);
 	}
 
-	drawOverlayAnimation (surface, dest, zoomFactor, frameNr, alphaEffectValue && cSettings::getInstance ().isAlphaEffects () ? alphaEffectValue : 254);
+	drawOverlayAnimation (surface, dest, zoomFactor, alphaEffectValue && cSettings::getInstance ().isAlphaEffects () ? alphaEffectValue : 254);
 }
 
 void cVehicle::render_BuildingOrBigClearing (const cMap& map, unsigned long long animationTime, SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, bool drawShadow) const
@@ -156,7 +161,7 @@ void cVehicle::render_BuildingOrBigClearing (const cMap& map, unsigned long long
 
 	// draw shadow
 	tmp = dest;
-    if (drawShadow) blitWithPreScale (uiData->build_shw_org.get (), uiData->build_shw.get (), NULL, surface, &tmp, zoomFactor);
+	if (drawShadow) blitWithPreScale (uiData->build_shw_org.get (), uiData->build_shw.get (), NULL, surface, &tmp, zoomFactor);
 
 	// draw player color
 	SDL_Rect src;
@@ -164,7 +169,7 @@ void cVehicle::render_BuildingOrBigClearing (const cMap& map, unsigned long long
 	src.h = src.w = (int) (uiData->build_org->h * zoomFactor);
 	src.x = (animationTime % 4) * src.w;
 	SDL_BlitSurface (getOwner ()->getColor ().getTexture (), NULL, GraphicsData.gfx_tmp.get (), NULL);
-    blitWithPreScale (uiData->build_org.get (), uiData->build.get (), &src, GraphicsData.gfx_tmp.get (), NULL, zoomFactor, 4);
+	blitWithPreScale (uiData->build_org.get (), uiData->build.get (), &src, GraphicsData.gfx_tmp.get (), NULL, zoomFactor, 4);
 
 	// draw vehicle
 	src.x = 0;
@@ -181,7 +186,7 @@ void cVehicle::render_smallClearing (unsigned long long animationTime, SDL_Surfa
 	// draw shadow
 	SDL_Rect tmp = dest;
 	if (drawShadow)
-        blitWithPreScale (uiData->clear_small_shw_org.get (), uiData->clear_small_shw.get (), NULL, surface, &tmp, zoomFactor);
+		blitWithPreScale (uiData->clear_small_shw_org.get (), uiData->clear_small_shw.get (), NULL, surface, &tmp, zoomFactor);
 
 	// draw player color
 	SDL_Rect src;
@@ -189,7 +194,7 @@ void cVehicle::render_smallClearing (unsigned long long animationTime, SDL_Surfa
 	src.h = src.w = (int) (uiData->clear_small_org->h * zoomFactor);
 	src.x = (animationTime % 4) * src.w;
 	SDL_BlitSurface (getOwner ()->getColor ().getTexture (), NULL, GraphicsData.gfx_tmp.get (), NULL);
-    blitWithPreScale (uiData->clear_small_org.get (), uiData->clear_small.get (), &src, GraphicsData.gfx_tmp.get (), NULL, zoomFactor, 4);
+	blitWithPreScale (uiData->clear_small_org.get (), uiData->clear_small.get (), &src, GraphicsData.gfx_tmp.get (), NULL, zoomFactor, 4);
 
 	// draw vehicle
 	src.x = 0;
@@ -204,7 +209,7 @@ void cVehicle::render_shadow (const cStaticMap& map, SDL_Surface* surface, const
 	if (map.isWater (getPosition()) && (data.isStealthOn & TERRAIN_SEA)) return;
 
 	if (alphaEffectValue && cSettings::getInstance ().isAlphaEffects ()) SDL_SetSurfaceAlphaMod (uiData->shw[dir].get (), alphaEffectValue / 5);
-    else SDL_SetSurfaceAlphaMod (uiData->shw[dir].get (), 50);
+	else SDL_SetSurfaceAlphaMod (uiData->shw[dir].get (), 50);
 	SDL_Rect tmp = dest;
 
 	// draw shadow
@@ -220,32 +225,40 @@ void cVehicle::render_shadow (const cStaticMap& map, SDL_Surface* surface, const
 	{
 		const Uint16 size = (int) (uiData->img_org[dir]->h * zoomFactor);
 		SDL_Rect r = {Sint16 (WalkFrame * size), 0, size, size};
-        blitWithPreScale (uiData->shw_org[dir].get (), uiData->shw[dir].get (), &r, surface, &tmp, zoomFactor);
+		blitWithPreScale (uiData->shw_org[dir].get (), uiData->shw[dir].get (), &r, surface, &tmp, zoomFactor);
 	}
 	else
-        blitWithPreScale (uiData->shw_org[dir].get (), uiData->shw[dir].get (), NULL, surface, &tmp, zoomFactor);
+		blitWithPreScale (uiData->shw_org[dir].get (), uiData->shw[dir].get (), NULL, surface, &tmp, zoomFactor);
 }
 
 void cVehicle::render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int alpha) const
 {
+	render_simple (surface, dest, zoomFactor, data, *uiData, getOwner (), dir, WalkFrame, alpha);
+}
+
+/*static*/ void cVehicle::render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, const sUnitData& data, const sVehicleUIData& uiData, const cPlayer* owner, int dir, int walkFrame, int alpha)
+{
 	// draw player color
-	SDL_BlitSurface (getOwner ()->getColor ().getTexture (), NULL, GraphicsData.gfx_tmp.get (), NULL);
+	if (owner)
+	{
+		SDL_BlitSurface (owner->getColor ().getTexture (), NULL, GraphicsData.gfx_tmp.get (), NULL);
+	}
 
 	// read the size:
 	SDL_Rect src;
-	src.w = (int) (uiData->img_org[dir]->w * zoomFactor);
-	src.h = (int) (uiData->img_org[dir]->h * zoomFactor);
+	src.w = (int) (uiData.img_org[dir]->w * zoomFactor);
+	src.h = (int) (uiData.img_org[dir]->h * zoomFactor);
 
 	if (data.animationMovement)
 	{
 		SDL_Rect tmp;
-		src.w = src.h = tmp.h = tmp.w = (int) (uiData->img_org[dir]->h * zoomFactor);
-		tmp.x = WalkFrame * tmp.w;
+		src.w = src.h = tmp.h = tmp.w = (int) (uiData.img_org[dir]->h * zoomFactor);
+		tmp.x = walkFrame * tmp.w;
 		tmp.y = 0;
-        blitWithPreScale (uiData->img_org[dir].get (), uiData->img[dir].get (), &tmp, GraphicsData.gfx_tmp.get (), NULL, zoomFactor);
+		blitWithPreScale (uiData.img_org[dir].get (), uiData.img[dir].get (), &tmp, GraphicsData.gfx_tmp.get (), NULL, zoomFactor);
 	}
 	else
-        blitWithPreScale (uiData->img_org[dir].get (), uiData->img[dir].get (), NULL, GraphicsData.gfx_tmp.get (), NULL, zoomFactor);
+		blitWithPreScale (uiData.img_org[dir].get (), uiData.img[dir].get (), NULL, GraphicsData.gfx_tmp.get (), NULL, zoomFactor);
 
 	// draw the vehicle
 	src.x = 0;
@@ -1489,7 +1502,7 @@ void sVehicleUIData::scaleSurfaces (float factor)
 }
 
 //-----------------------------------------------------------------------------
-void cVehicle::blitWithPreScale (SDL_Surface* org_src, SDL_Surface* src, SDL_Rect* srcrect, SDL_Surface* dest, SDL_Rect* destrect, float factor, int frames) const
+void cVehicle::blitWithPreScale (SDL_Surface* org_src, SDL_Surface* src, SDL_Rect* srcrect, SDL_Surface* dest, SDL_Rect* destrect, float factor, int frames)
 {
 	if (!cSettings::getInstance().shouldDoPrescale())
 	{

src/game/data/units/building.cpp
@@ -388,7 +388,19 @@ void cBuilding::render_beton (SDL_Surface* surface, const SDL_Rect& dest, float 
 	}
 }
 
-void cBuilding::render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int frameNr, int alpha) const
+void cBuilding::render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int animationTime, int alpha) const
+{
+	int frameNr = dir;
+	if (data.hasFrames && data.isAnimated && cSettings::getInstance ().isAnimations () &&
+		isDisabled () == false)
+	{
+		frameNr = (animationTime % data.hasFrames);
+	}
+
+	render_simple (surface, dest, zoomFactor, data, *uiData, getOwner (), frameNr, alpha);
+}
+
+/*static*/ void cBuilding::render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, const sUnitData& data, const sBuildingUIData& uiData, const cPlayer* owner, int frameNr, int alpha)
 {
 	// read the size:
 	SDL_Rect src;
@@ -401,39 +413,39 @@ void cBuilding::render_simple (SDL_Surface* surface, const SDL_Rect& dest, float
 	}
 	else
 	{
-		src.w = (int) (uiData->img_org->w * zoomFactor);
-		src.h = (int) (uiData->img_org->h * zoomFactor);
+		src.w = (int) (uiData.img_org->w * zoomFactor);
+		src.h = (int) (uiData.img_org->h * zoomFactor);
 	}
 
 	// blit the players color and building graphic
-	if (data.hasPlayerColor) SDL_BlitSurface (getOwner ()->getColor ().getTexture (), NULL, GraphicsData.gfx_tmp.get (), NULL);
+	if (data.hasPlayerColor && owner) SDL_BlitSurface (owner->getColor ().getTexture (), NULL, GraphicsData.gfx_tmp.get (), NULL);
 	else SDL_FillRect (GraphicsData.gfx_tmp.get (), NULL, 0x00FF00FF);
 
 	if (data.hasFrames)
 	{
 		src.x = frameNr * Round (64.0f * zoomFactor);
 
-		CHECK_SCALING (*uiData->img, *uiData->img_org, zoomFactor);
-		SDL_BlitSurface (uiData->img.get (), &src, GraphicsData.gfx_tmp.get (), NULL);
+		CHECK_SCALING (*uiData.img, *uiData.img_org, zoomFactor);
+		SDL_BlitSurface (uiData.img.get (), &src, GraphicsData.gfx_tmp.get (), NULL);
 
 		src.x = 0;
 	}
 	else if (data.hasClanLogos)
 	{
-		CHECK_SCALING (*uiData->img, *uiData->img_org, zoomFactor);
+		CHECK_SCALING (*uiData.img, *uiData.img_org, zoomFactor);
 		src.x = 0;
 		src.y = 0;
 		src.w = (int) (128 * zoomFactor);
 		src.h = (int) (128 * zoomFactor);
 		// select clan image
-		if (getOwner ()->getClan () != -1)
-			src.x = (int)((getOwner ()->getClan () + 1) * 128 * zoomFactor);
-		SDL_BlitSurface (uiData->img.get (), &src, GraphicsData.gfx_tmp.get (), NULL);
+		if (owner && owner->getClan () != -1)
+			src.x = (int)((owner->getClan () + 1) * 128 * zoomFactor);
+		SDL_BlitSurface (uiData.img.get (), &src, GraphicsData.gfx_tmp.get (), NULL);
 	}
 	else
 	{
-		CHECK_SCALING (*uiData->img, *uiData->img_org, zoomFactor);
-		SDL_BlitSurface (uiData->img.get (), NULL, GraphicsData.gfx_tmp.get (), NULL);
+		CHECK_SCALING (*uiData.img, *uiData.img_org, zoomFactor);
+		SDL_BlitSurface (uiData.img.get (), NULL, GraphicsData.gfx_tmp.get (), NULL);
 	}
 
 	// draw the building
@@ -484,14 +496,7 @@ void cBuilding::render (unsigned long long animationTime, SDL_Surface* surface, 
 		blittAlphaSurface (uiData->shw.get (), NULL, surface, &tmp);
 	}
 
-	int frameNr = dir;
-	if (data.hasFrames && data.isAnimated && cSettings::getInstance().isAnimations() &&
-		isDisabled() == false)
-	{
-		frameNr = (animationTime % data.hasFrames);
-	}
-
-	render_simple (surface, dest, zoomFactor, frameNr, alphaEffectValue && cSettings::getInstance ().isAlphaEffects () ? alphaEffectValue : 254);
+	render_simple (surface, dest, zoomFactor, animationTime, alphaEffectValue && cSettings::getInstance ().isAlphaEffects () ? alphaEffectValue : 254);
 }
 
 //--------------------------------------------------------------------------

src/game/data/units/building.h
@@ -222,7 +222,8 @@ public:
 	* draws the main image of the building onto the given surface
 	*/
 	void render (unsigned long long animationTime, SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, bool drawShadow, bool drawConcrete) const;
-	void render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int frameNr, int alpha = 254) const;
+	void render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int animationTime = 0, int alpha = 254) const;
+	static void render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, const sUnitData& data, const sBuildingUIData& uiData, const cPlayer* owner, int frameNr = 0, int alpha = 254);
 
 	void executeUpdateBuildingCommmand (const cClient& client, bool updateAllOfSameType) const;
 

src/game/data/units/vehicle.h
@@ -236,7 +236,7 @@ public:
 	void clearDetectedInThisTurnPlayerList();
 	bool wasDetectedInThisTurnByPlayer (const cPlayer* player) const;
 
-	void blitWithPreScale (SDL_Surface* org_src, SDL_Surface* src, SDL_Rect* srcrect, SDL_Surface* dest, SDL_Rect* destrect, float factor, int frames = 1) const;
+	static void blitWithPreScale (SDL_Surface* org_src, SDL_Surface* src, SDL_Rect* srcrect, SDL_Surface* dest, SDL_Rect* destrect, float factor, int frames = 1);
 
 	void executeAutoMoveJobCommand (cClient& client);
 	void executeLayMinesCommand (const cClient& client);
@@ -255,12 +255,14 @@ public:
 	*/
 	void render (const cMap* map, unsigned long long animationTime, const cPlayer* activePlayer, SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, bool drawShadow) const;
 	void render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int alpha = 254) const;
+	static void render_simple (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, const sUnitData& data, const sVehicleUIData& uiData, const cPlayer* owner, int dir = 0, int walkFrame = 0, int alpha = 254);
 	/**
 	* draws the overlay animation of the vehicle on the given surface
 	*@author: eiko
 	*/
 	void drawOverlayAnimation (unsigned long long animationTime, SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor) const;
 	void drawOverlayAnimation (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, int frameNr, int alpha = 254) const;
+	static void drawOverlayAnimation (SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, const sUnitData& data, const sVehicleUIData& uiData, int frameNr = 0, int alpha = 254);
 
 	bool isUnitLoaded () const { return loaded; }
 

src/ui/graphical/menu/widgets/special/reportdisadvantageslistviewitem.cpp
@@ -36,14 +36,24 @@ cReportDisadvantagesListViewItem::cReportDisadvantagesListViewItem (const sID& u
 {
 	const auto totalHeight = std::max (unitImageHeight, (int)(disadvantages.size () / maxItemsInRow + 1) * font->getFontHeight ());
 
-	AutoSurface unitSurface;
-	if (unitId.isABuilding ())
+	AutoSurface unitSurface (SDL_CreateRGBSurface (0, unitImageWidth, unitImageHeight, Video.getColDepth (), 0, 0, 0, 0));
+	SDL_SetColorKey (unitSurface.get (), SDL_TRUE, 0x00FF00FF);
+	SDL_FillRect (unitSurface.get (), nullptr, 0x00FF00FF);
+	SDL_Rect dest = {0, 0, 0, 0};
+
+	const auto& data = *unitId.getUnitDataOriginalVersion ();
+	if (unitId.isAVehicle ())
 	{
-        unitSurface = AutoSurface (scaleSurface (UnitsData.getBuildingUI (unitId)->img_org.get (), nullptr, unitImageWidth, unitImageHeight));
+		const float zoomFactor = unitImageWidth / 64.0f;
+		const auto& uiData = *UnitsData.getVehicleUI (unitId);
+		cVehicle::render_simple (unitSurface.get (), dest, zoomFactor, data, uiData, nullptr);
+		cVehicle::drawOverlayAnimation (unitSurface.get (), dest, zoomFactor, data, uiData);
 	}
-	else if (unitId.isAVehicle ())
+	else if (unitId.isABuilding ())
 	{
-        unitSurface = AutoSurface (scaleSurface (UnitsData.getVehicleUI (unitId)->img_org[0].get (), nullptr, unitImageWidth, unitImageHeight));
+		const float zoomFactor = unitImageWidth / (data.isBig ? 128.0f : 64.0f);
+		const auto& uiData = *UnitsData.getBuildingUI (unitId);
+		cBuilding::render_simple (unitSurface.get (), dest, zoomFactor, data, uiData, nullptr);
 	}
 	auto unitImage = addChild (std::make_unique<cImage> (cPosition (0, (totalHeight - unitImageHeight) / 2), unitSurface.get ()));
 

src/ui/graphical/menu/widgets/special/reportmessagelistviewitem.cpp
@@ -43,14 +43,24 @@ cReportMessageListViewItem::cReportMessageListViewItem (const cSavedReport& repo
 
 		const auto totalHeight = std::max (unitImageSize, textLabel->getSize ().y ());
 
-		AutoSurface unitSurface;
-		if (unitId.isABuilding ())
+		AutoSurface unitSurface (SDL_CreateRGBSurface (0, unitImageSize, unitImageSize, Video.getColDepth (), 0, 0, 0, 0));
+		SDL_SetColorKey (unitSurface.get (), SDL_TRUE, 0x00FF00FF);
+		SDL_FillRect (unitSurface.get (), nullptr, 0x00FF00FF);
+		SDL_Rect dest = {0, 0, 0, 0};
+
+		const auto& data = *unitId.getUnitDataOriginalVersion ();
+		if (unitId.isAVehicle ())
 		{
-			unitSurface = AutoSurface (scaleSurface (UnitsData.getBuildingUI (unitId)->img_org.get (), nullptr, unitImageSize, unitImageSize));
+			const float zoomFactor = unitImageSize / 64.0f;
+			const auto& uiData = *UnitsData.getVehicleUI (unitId);
+			cVehicle::render_simple (unitSurface.get (), dest, zoomFactor, data, uiData, nullptr);
+			cVehicle::drawOverlayAnimation (unitSurface.get (), dest, zoomFactor, data, uiData);
 		}
-		else if (unitId.isAVehicle ())
+		else if (unitId.isABuilding ())
 		{
-			unitSurface = AutoSurface (scaleSurface (UnitsData.getVehicleUI (unitId)->img_org[0].get (), nullptr, unitImageSize, unitImageSize));
+			const float zoomFactor = unitImageSize / (data.isBig ? 128.0f : 64.0f);
+			const auto& uiData = *UnitsData.getBuildingUI (unitId);
+			cBuilding::render_simple (unitSurface.get (), dest, zoomFactor, data, uiData, nullptr);
 		}
 		addChild (std::make_unique<cImage> (cPosition (0, (totalHeight - unitImageSize) / 2), unitSurface.get ()));
 	}

src/ui/graphical/menu/widgets/special/reportunitlistviewitem.cpp
@@ -33,31 +33,31 @@ cReportUnitListViewItem::cReportUnitListViewItem (cUnit& unit_) :
 	unit (unit_)
 {
 	const int unitImageSize = 32;
-	auto surface = SDL_CreateRGBSurface (0, unitImageSize, unitImageSize, Video.getColDepth (), 0, 0, 0, 0);
-	SDL_SetColorKey (surface, SDL_TRUE, 0x00FF00FF);
-	SDL_FillRect (surface, NULL, 0x00FF00FF);
+	AutoSurface surface(SDL_CreateRGBSurface (0, unitImageSize, unitImageSize, Video.getColDepth (), 0, 0, 0, 0));
+	SDL_SetColorKey (surface.get (), SDL_TRUE, 0x00FF00FF);
+	SDL_FillRect (surface.get (), nullptr, 0x00FF00FF);
 	SDL_Rect dest = {0, 0, 0, 0};
 
 	if (unit.data.ID.isAVehicle ())
 	{
 		const auto& vehicle = static_cast<const cVehicle&>(unit);
 		const float zoomFactor = unitImageSize / 64.0f;
-		vehicle.render_simple (surface, dest, zoomFactor);
-		vehicle.drawOverlayAnimation (surface, dest, zoomFactor, 0);
+		vehicle.render_simple (surface.get (), dest, zoomFactor);
+		vehicle.drawOverlayAnimation (surface.get (), dest, zoomFactor, 0);
 	}
 	else if (unit.data.ID.isABuilding ())
 	{
 		const auto& building = static_cast<const cBuilding&>(unit);
 		const float zoomFactor = unitImageSize / (building.data.isBig ? 128.0f : 64.0f);
-		building.render_simple (surface, dest, zoomFactor, 0);
+		building.render_simple (surface.get (), dest, zoomFactor, 0);
 	}
-	else surface = NULL;
+	else surface = nullptr;
 
 	auto unitDetails = addChild (std::make_unique<cUnitDetailsHud> (cBox<cPosition> (cPosition (unitImageSize+3+75+3, 0), cPosition (unitImageSize+3+75+3 + 155, 48)), true));
 	unitDetails->setPlayer (unit.getOwner ());
 	unitDetails->setUnit (&unit);
 
-	unitImage = addChild (std::make_unique<cImage> (cPosition (0, (unitDetails->getSize ().y () - unitImageSize)/2), surface));
+	unitImage = addChild (std::make_unique<cImage> (cPosition (0, (unitDetails->getSize ().y () - unitImageSize)/2), surface.get ()));
 	unitImage->setConsumeClick (false);
 
 	auto nameLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (unitImage->getEndPosition ().x ()+3, 0), cPosition (unitDetails->getPosition ().x ()-3, unitDetails->getEndPosition ().y ())), unit.getDisplayName (), FONT_LATIN_NORMAL, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));

src/ui/graphical/menu/widgets/special/unitlistviewitem.cpp
@@ -31,31 +31,28 @@ cUnitListViewItem::cUnitListViewItem (unsigned int width, const sID& unitId_, co
 	unitId (unitId_)
 {
 	const int unitImageSize = 32;
-	auto surface = SDL_CreateRGBSurface (0, unitImageSize, unitImageSize, Video.getColDepth (), 0, 0, 0, 0);
-	SDL_SetColorKey (surface, SDL_TRUE, 0x00FF00FF);
-	SDL_FillRect (surface, NULL, 0x00FF00FF);
+	AutoSurface surface (SDL_CreateRGBSurface (0, unitImageSize, unitImageSize, Video.getColDepth (), 0, 0, 0, 0));
+	SDL_SetColorKey (surface.get (), SDL_TRUE, 0x00FF00FF);
+	SDL_FillRect (surface.get (), nullptr, 0x00FF00FF);
 	SDL_Rect dest = {0, 0, 0, 0};
 
-	// TODO: very very bad...
-	//        why do we need to create a full vehicle/building object to draw it?!
-	//        Currently the const_cast is okay here because the player state is not changed during the render
-	//        method of the units.
+	const auto& data = *unitId.getUnitDataOriginalVersion ();
 	if (unitId.isAVehicle ())
 	{
-		cVehicle vehicle (*unitId.getUnitDataOriginalVersion (), const_cast<cPlayer*>(&owner), 0);
 		const float zoomFactor = unitImageSize / 64.0f;
-		vehicle.render_simple (surface, dest, zoomFactor);
-		vehicle.drawOverlayAnimation (surface, dest, zoomFactor, 0);
+		const auto& uiData = *UnitsData.getVehicleUI (unitId);
+		cVehicle::render_simple (surface.get (), dest, zoomFactor, data, uiData, &owner);
+		cVehicle::drawOverlayAnimation (surface.get (), dest, zoomFactor, data, uiData);
 	}
 	else if (unitId.isABuilding ())
 	{
-		cBuilding building (unitId.getUnitDataOriginalVersion (), const_cast<cPlayer*>(&owner), 0);
-		const float zoomFactor = unitImageSize / (building.data.isBig ? 128.0f : 64.0f);
-		building.render_simple (surface, dest, zoomFactor, 0);
+		const float zoomFactor = unitImageSize / (data.isBig ? 128.0f : 64.0f);
+		const auto& uiData = *UnitsData.getBuildingUI (unitId);
+		cBuilding::render_simple (surface.get (), dest, zoomFactor, data, uiData, &owner);
 	}
-	else surface = NULL;
+	else surface = nullptr;
 
-	unitImage = addChild (std::make_unique<cImage> (cPosition (0, 0), surface));
+	unitImage = addChild (std::make_unique<cImage> (cPosition (0, 0), surface.get ()));
 	unitImage->setConsumeClick (false);
 
 	nameLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (unitImage->getEndPosition ().x ()+3, 0), cPosition (width, unitImage->getEndPosition ().y ())), owner.getUnitDataCurrentVersion (unitId)->name, FONT_LATIN_SMALL_WHITE, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));

commit 3e1e1cc41a1ff7478bd66cfac2852dc4a1802426

fixed #918  : missing overlay animation of vehicles (was broken since [91abd47]).

src/game/data/units/vehicle.cpp
@@ -137,8 +137,13 @@ void cVehicle::drawOverlayAnimation (SDL_Surface* surface, const SDL_Rect& dest,
 void cVehicle::drawOverlayAnimation (unsigned long long animationTime, SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor) const
 {
 	if (data.hasOverlay == false || cSettings::getInstance().isAnimations() == false) return;
+	int frameNr = 0;
+	if (isDisabled() == false)
+	{
+		frameNr = animationTime % (uiData->overlay_org->w / uiData->overlay_org->h);
+	}
 
-	drawOverlayAnimation (surface, dest, zoomFactor, alphaEffectValue && cSettings::getInstance().isAlphaEffects() ? alphaEffectValue : 254);
+	drawOverlayAnimation(surface, dest, zoomFactor, frameNr, alphaEffectValue && cSettings::getInstance().isAlphaEffects() ? alphaEffectValue : 254);
 }
 
 void cVehicle::render_BuildingOrBigClearing (const cMap& map, unsigned long long animationTime, SDL_Surface* surface, const SDL_Rect& dest, float zoomFactor, bool drawShadow) const