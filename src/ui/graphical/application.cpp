87dca782f5ed3ac08a01ba3c3bb4d0341b82d93c
Albert Ziegenhagel <alzi@maxr.org>
2016-10-06 15:39:26 +0200

    Changed shortcut to toggle debug frames from CTRL+w to CTRL+ALT+w to reduce likelihood of enabling it by accident.

--- src/ui/graphical/application.cpp
+++ src/ui/graphical/application.cpp
@@ -62,7 +62,7 @@ cApplication::cApplication() :
 		if (!shouldDrawFramesPerSecond) drawFramesPerSecond (0, false); // make sure the last fps will not be visible
 	});
 
-	const auto widgetFramesShortcut = addShortcut (std::make_unique<cShortcut> (cKeySequence (cKeyCombination (toEnumFlag (eKeyModifierType::Ctrl), SDLK_w))));
+	const auto widgetFramesShortcut = addShortcut (std::make_unique<cShortcut> (cKeySequence (cKeyCombination (toEnumFlag (eKeyModifierType::Ctrl) | eKeyModifierType::Alt, SDLK_w))));
 	signalConnectionManager.connect (widgetFramesShortcut->triggered, [this]()
 	{
 		cWidget::toggleDrawDebugFrames();
