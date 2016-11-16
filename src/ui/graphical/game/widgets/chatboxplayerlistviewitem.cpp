maxr-dot-org/maxr

commit 7f7ad6ced9f33217676f52051419918bfdbd2013

Added click signal to label

src/ui/graphical/menu/widgets/label.cpp
@@ -22,7 +22,7 @@
 
 //------------------------------------------------------------------------------
 cLabel::cLabel (const cBox<cPosition>& area, const std::string& text_, eUnicodeFontType fontType_, AlignmentFlags alignment_) :
-	cWidget (area),
+	cClickableWidget (area),
 	fontType (fontType_),
 	alignment (alignment_),
 	wordWrap (false)
@@ -264,4 +264,12 @@ void cLabel::handleResized (const cPosition& oldSize)
 	SDL_SetColorKey (surface.get(), SDL_TRUE, 0xFF00FF);
 
 	updateDisplayInformation();
+}
+
+//------------------------------------------------------------------------------
+bool cLabel::handleClicked (cApplication& application, cMouse& mouse, eMouseButtonType button)
+{
+	clicked ();
+
+	return false;
 }

src/ui/graphical/menu/widgets/label.h
@@ -24,11 +24,11 @@
 #include <vector>
 
 #include "maxrconfig.h"
-#include "ui/graphical/widget.h"
+#include "ui/graphical/menu/widgets/clickablewidget.h"
 #include "ui/graphical/alignment.h"
 #include "unifonts.h"
 
-class cLabel : public cWidget
+class cLabel : public cClickableWidget
 {
 public:
 	cLabel (const cBox<cPosition>& area, const std::string& text, eUnicodeFontType fontType_ = FONT_LATIN_NORMAL, AlignmentFlags alignment = toEnumFlag (eAlignmentType::Left)  | eAlignmentType::Top);
@@ -45,6 +45,11 @@ public:
 	virtual void draw (SDL_Surface& destination, const cBox<cPosition>& clipRect) MAXR_OVERRIDE_FUNCTION;
 
 	virtual void handleResized (const cPosition& oldSize) MAXR_OVERRIDE_FUNCTION;
+
+	cSignal<void ()> clicked;
+protected:
+
+	virtual bool handleClicked (cApplication& application, cMouse& mouse, eMouseButtonType button) MAXR_OVERRIDE_FUNCTION;
 private:
 	std::string text;
 	eUnicodeFontType fontType;

commit 29017e59d6f645d3d30071fbec218d1a8094f92e

Fixed problem introduced in 7f7ad6ce
Labels have been made clickable in that commit which made them consume click events. This commit disables consuming the click event of labels wherever it is not desired.

src/ui/graphical/game/widgets/chatboxplayerlistviewitem.cpp
@@ -35,6 +35,7 @@ cChatBoxPlayerListViewItem::cChatBoxPlayerListViewItem (const cPlayer& player_) 
 	updatePlayerFinishedTurn();
 
 	nameLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (getPosition() + cPosition (colorImage->getEndPosition().x() + 4, 0), getPosition() + cPosition (getSize().x() - readyImage->getSize().x(), readyImage->getSize().y())), player->getName()));
+	nameLabel->setConsumeClick (false);
 
 	fitToChildren();
 

src/ui/graphical/game/widgets/gamemessagelistviewitem.cpp
@@ -32,6 +32,7 @@ cGameMessageListViewItem::cGameMessageListViewItem (const std::string& message, 
 	messageLabel = addChild (std::make_unique<cLabel> (labelArea, message));
 	messageLabel->setWordWrap (true);
 	messageLabel->resizeToTextHeight();
+	messageLabel->setConsumeClick (false);
 
 	resize (cPosition (getSize().x(), messageLabel->getSize().y() - 1 + beginMargin.y() + endMargin.y()));
 

src/ui/graphical/menu/widgets/label.cpp
@@ -271,5 +271,5 @@ bool cLabel::handleClicked (cApplication& application, cMouse& mouse, eMouseButt
 {
 	clicked ();
 
-	return false;
+	return true;
 }

src/ui/graphical/menu/widgets/special/lobbychatboxlistviewitem.cpp
@@ -29,6 +29,7 @@ cLobbyChatBoxListViewItem::cLobbyChatBoxListViewItem (const std::string& text) :
 	messageLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (getPosition(), getPosition() + cPosition (getSize().x() - 1, 0)), text));
 	messageLabel->setWordWrap (true);
 	messageLabel->resizeToTextHeight();
+	messageLabel->setConsumeClick (false);
 
 	fitToChildren();
 }
@@ -48,6 +49,7 @@ cLobbyChatBoxListViewItem::cLobbyChatBoxListViewItem (const std::string& prefix,
 		const auto prefixText = addColon ? prefix + lngPack.i18n ("Text~Punctuation~Colon") : prefix;
 		prefixTextWidth = std::max (desiredPrefixTextWidth, font->getTextWide (prefixText));
 		prefixLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (getPosition (), getPosition () + cPosition (prefixTextWidth, 10)), prefixText));
+		prefixLabel->setConsumeClick (false);
 	}
 	else
 	{
@@ -61,6 +63,7 @@ cLobbyChatBoxListViewItem::cLobbyChatBoxListViewItem (const std::string& prefix,
 	messageLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (messageLabelBeginPos, messageLabelEndPos), text));
 	messageLabel->setWordWrap (true);
 	messageLabel->resizeToTextHeight ();
+	messageLabel->setConsumeClick (false);
 
 	fitToChildren ();
 }
@@ -102,6 +105,7 @@ void cLobbyChatBoxListViewItem::setDesiredPrefixLabelWidth (int width)
 	messageLabel->resize (cPosition (getSize ().x () - prefixTextWidth, messageLabel->getSize ().y ()));
 	messageLabel->moveTo (getPosition () + cPosition (prefixTextWidth, 0));
 	messageLabel->resizeToTextHeight ();
+	messageLabel->setConsumeClick (false);
 
 	fitToChildren ();
 }

src/ui/graphical/menu/widgets/special/lobbyplayerlistviewitem.cpp
@@ -38,6 +38,7 @@ cLobbyPlayerListViewItem::cLobbyPlayerListViewItem (std::shared_ptr<cPlayerBasic
 	updatePlayerReady();
 
 	nameLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (getPosition() + cPosition (colorImage->getEndPosition().x() + 4, 0), cPosition (readyImage->getPosition().x(), readyImage->getEndPosition().y())), player->getName()));
+	nameLabel->setConsumeClick (false);
 
 	fitToChildren();
 

src/ui/graphical/menu/widgets/special/reportdisadvantageslistviewitem.cpp
@@ -59,6 +59,7 @@ cReportDisadvantagesListViewItem::cReportDisadvantagesListViewItem (const sID& u
 
 	auto nameLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (unitImage->getEndPosition().x(), 0), cPosition (unitImage->getEndPosition().x() + unitNameWidth, totalHeight)), unitId.getUnitDataOriginalVersion()->name, FONT_LATIN_NORMAL, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));
 	nameLabel->setWordWrap (true);
+	nameLabel->setConsumeClick (false);
 
 	for (size_t i = 0; i < disadvantages.size(); ++i)
 	{
@@ -67,7 +68,8 @@ cReportDisadvantagesListViewItem::cReportDisadvantagesListViewItem (const sID& u
 		const int row = static_cast<int> (i / maxItemsInRow);
 		const int col = static_cast<int> (i % maxItemsInRow);
 
-		addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (nameLabel->getEndPosition().x() + casualityLabelWidth * col + (row % 2 == 0 ? 15 : 0), font->getFontHeight()*row), cPosition (nameLabel->getEndPosition().x() + casualityLabelWidth * (col + 1) + (row % 2 == 0 ? 15 : 0), font->getFontHeight() * (row + 1))), iToStr (disadvantage), FONT_LATIN_NORMAL, eAlignmentType::Center));
+		auto disadvantageLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (nameLabel->getEndPosition().x() + casualityLabelWidth * col + (row % 2 == 0 ? 15 : 0), font->getFontHeight()*row), cPosition (nameLabel->getEndPosition().x() + casualityLabelWidth * (col + 1) + (row % 2 == 0 ? 15 : 0), font->getFontHeight() * (row + 1))), iToStr (disadvantage), FONT_LATIN_NORMAL, eAlignmentType::Center));
+		disadvantageLabel->setConsumeClick (false);
 	}
 
 	fitToChildren();

src/ui/graphical/menu/widgets/special/reportmessagelistviewitem.cpp
@@ -36,6 +36,7 @@ cReportMessageListViewItem::cReportMessageListViewItem (const cSavedReport& repo
 	auto textLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (unitImageSize, 0), cPosition (450, 0)), report.getMessage(), FONT_LATIN_NORMAL, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));
 	textLabel->setWordWrap (true);
 	textLabel->resizeToTextHeight();
+	textLabel->setConsumeClick (false);
 
 	if (report.hasUnitId())
 	{

src/ui/graphical/menu/widgets/special/reportunitlistviewitem.cpp
@@ -62,10 +62,13 @@ cReportUnitListViewItem::cReportUnitListViewItem (cUnit& unit_) :
 
 	auto nameLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (unitImage->getEndPosition().x() + 3, 0), cPosition (unitDetails->getPosition().x() - 3, unitDetails->getEndPosition().y())), unit.getDisplayName(), FONT_LATIN_NORMAL, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));
 	nameLabel->setWordWrap (true);
+	nameLabel->setConsumeClick (false);
 
 	auto positionLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (unitDetails->getEndPosition().x() + 5, 0), cPosition (unitDetails->getEndPosition().x() + 5 + 50, unitDetails->getEndPosition().y())), iToStr (unit.getPosition().x()) + "," + iToStr (unit.getPosition().y()), FONT_LATIN_NORMAL, toEnumFlag (eAlignmentType::CenterHorizontal) | eAlignmentType::CenterVerical));
+	positionLabel->setConsumeClick (false);
 
-	addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (positionLabel->getEndPosition().x(), 0), cPosition (positionLabel->getEndPosition().x() + 120, unitDetails->getEndPosition().y())), unit.getStatusStr (unit.getOwner()), FONT_LATIN_NORMAL, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));
+	auto statusLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (positionLabel->getEndPosition().x(), 0), cPosition (positionLabel->getEndPosition().x() + 120, unitDetails->getEndPosition().y())), unit.getStatusStr (unit.getOwner()), FONT_LATIN_NORMAL, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));
+	statusLabel->setConsumeClick (false);
 
 	fitToChildren();
 }

src/ui/graphical/menu/widgets/special/saveslotwidget.cpp
@@ -35,8 +35,11 @@ cSaveSlotWidget::cSaveSlotWidget (const cPosition& position) :
 	renameable (false)
 {
 	numberLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (getPosition() + cPosition (13, 28), getPosition() + cPosition (13 + 20, 28 + 15)), "", FONT_LATIN_BIG));
+	numberLabel->setConsumeClick (false);
 	timeLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (getPosition() + cPosition (42, 19), getPosition() + cPosition (42 + 98, 19 + 10)), ""));
+	timeLabel->setConsumeClick (false);
 	typeLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (getPosition() + cPosition (145, 19), getPosition() + cPosition (145 + 46, 19 + 10)), "", FONT_LATIN_NORMAL, eAlignmentType::CenterHorizontal));
+	typeLabel->setConsumeClick (false);
 	nameLineEdit = addChild (std::make_unique<cLineEdit> (cBox<cPosition> (getPosition() + cPosition (42, 42), getPosition() + cPosition (42 + 148, 42 + 10))));
 	nameLineEdit->setReadOnly (true);
 

src/ui/graphical/menu/widgets/special/textlistviewitem.cpp
@@ -26,6 +26,7 @@ cTextListViewItem::cTextListViewItem (const std::string& text) :
 	cAbstractListViewItem (cPosition (50, font->getFontHeight (FONT_LATIN_NORMAL)))
 {
 	label = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (0, 0), cPosition (getSize().x() - 1, font->getFontHeight (FONT_LATIN_NORMAL))), text));
+	label->setConsumeClick (false);
 }
 
 //------------------------------------------------------------------------------

src/ui/graphical/menu/widgets/special/unitlistviewitem.cpp
@@ -57,6 +57,7 @@ cUnitListViewItem::cUnitListViewItem (unsigned int width, const sID& unitId_, co
 
 	nameLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (unitImage->getEndPosition().x() + 3, 0), cPosition (width, unitImage->getEndPosition().y())), owner.getUnitDataCurrentVersion (unitId)->name, FONT_LATIN_SMALL_WHITE, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));
 	nameLabel->setWordWrap (true);
+	nameLabel->setConsumeClick (false);
 
 	auto size = unitImage->getArea();
 	size.add (nameLabel->getArea());

src/ui/graphical/menu/widgets/special/unitlistviewitembuy.cpp
@@ -26,7 +26,8 @@
 cUnitListViewItemBuy::cUnitListViewItemBuy (unsigned int width, const sID& unitId, const cPlayer& owner) :
 	cUnitListViewItem (width, unitId, owner)
 {
-	addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (width - 15, 0), cPosition (width, unitImage->getEndPosition().y())), iToStr (owner.getUnitDataCurrentVersion (unitId)->buildCosts), FONT_LATIN_SMALL_YELLOW, toEnumFlag (eAlignmentType::Right) | eAlignmentType::CenterVerical));
+	auto costLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (width - 15, 0), cPosition (width, unitImage->getEndPosition().y())), iToStr (owner.getUnitDataCurrentVersion (unitId)->buildCosts), FONT_LATIN_SMALL_YELLOW, toEnumFlag (eAlignmentType::Right) | eAlignmentType::CenterVerical));
+	costLabel->setConsumeClick (false);
 
 	nameLabel->resize (nameLabel->getSize() - cPosition (15, 0));
 }

src/ui/graphical/menu/widgets/special/unitlistviewitemcargo.cpp
@@ -33,6 +33,7 @@ cUnitListViewItemCargo::cUnitListViewItemCargo (unsigned int width, const sID& u
 	if (unitData->storeResType == sUnitData::STORE_RES_METAL || unitData->storeResType == sUnitData::STORE_RES_OIL)
 	{
 		cargoLabel = addChild (std::make_unique<cLabel> (cBox<cPosition> (cPosition (nameLabel->getPosition().x() + 15, nameLabel->getEndPosition().y() - 13), nameLabel->getEndPosition() - cPosition (0, 3)), iToStr (owner.getUnitDataCurrentVersion (unitId)->buildCosts), FONT_LATIN_SMALL_YELLOW, toEnumFlag (eAlignmentType::Left) | eAlignmentType::CenterVerical));
+		cargoLabel->setConsumeClick (false);
 
 		nameLabel->resize (nameLabel->getSize() - cPosition (0, 13));
 		nameLabel->setAlignment (toEnumFlag (eAlignmentType::Left) | eAlignmentType::Bottom);