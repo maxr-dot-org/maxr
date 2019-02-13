91a46c533b60392b2727ae114ea2078da5be5de7
Eiko Oltmanns <eiko@maxr.org>
2019-02-13 23:47:46 +0100

    fixed linux build

--- src/game/logic/jobs/planetakeoffjob.h
+++ src/game/logic/jobs/planetakeoffjob.h
@@ -25,6 +25,7 @@
 #include "utility/serialization/binaryarchive.h"
 #include "utility/serialization/xmlarchive.h"
 #include "utility/signal/signalconnectionmanager.h"
+#include "game/data/units/unit.h"
 
 class cVehicle;
 
