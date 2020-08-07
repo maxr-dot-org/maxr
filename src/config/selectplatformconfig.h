9f11e56a09d8ba85834953a1aaef77d3cd93585b
Jarod42 <Jarod42@users.noreply.github.com>
2020-08-07 14:56:12 +0200

    Apply commit https://github.com/AMDmi3/maxr/commit/3b8088e6ab357214773e5b7d365ea7edf1bd0aa8

--- src/config/selectplatformconfig.h
+++ src/config/selectplatformconfig.h
@@ -19,7 +19,7 @@
 
 #ifndef MAXR_PLATFORM_CONFIG
 
-#if (defined(linux) || defined(__linux) || defined(__linux__) || defined(__GNU__) || defined(__GLIBC__)) && !defined(_CRAYC)
+#if (defined(linux) || defined(__linux) || defined(__linux__) || defined(__GNU__) || defined(__GLIBC__) || defined(__FreeBSD__)) && !defined(_CRAYC)
 #   define MAXR_PLATFORM_CONFIG "config/platform/linux.h"
 #elif defined(__CYGWIN__)
 #   define MAXR_PLATFORM_CONFIG "config/platform/cygwin.h"
