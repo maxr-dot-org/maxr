0fdce8a1577034333c80eda490461432a3b3cc68
Jarod42 <Jarod42@users.noreply.github.com>
2020-09-04 12:06:06 +0200

    Add ranges::find_if

---
+++ src/utility/ranges.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *      Mechanized Assault and Exploration Reloaded Projectfile            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef utility_rangesH
+#define utility_rangesH
+
+#include <algorithm>
+
+namespace ranges
+{
+
+template <typename Range, typename Predicate>
+auto find_if(Range&& range, Predicate&& predicate)
+{
+	return std::find_if(std::begin (range), std::end (range), std::forward<Predicate>(predicate));
+}
+
+}
+
+#endif
