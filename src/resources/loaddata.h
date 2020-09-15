d196464802eca9fb3f2a5a98b1e84d1a8a4360ff
Jarod42 <Jarod42@users.noreply.github.com>
2020-09-15 12:37:33 +0200

    Move loaddata.cpp into resources/

--- src/resources/loaddata.h
+++ src/resources/loaddata.h
@@ -16,14 +16,9 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-//
-//
-//
-//
-///////////////////////////////////////////////////////////////////////////////
 
-#ifndef loaddataH
-#define loaddataH
+#ifndef resources_loaddataH
+#define resources_loaddataH
 
 enum class eLoadingState
 {
