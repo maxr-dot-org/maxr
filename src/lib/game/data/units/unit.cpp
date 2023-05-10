7395f48607bde49f60a192e6f073aba16b7c1771
Jarod42 <Jarod42@users.noreply.github.com>
2023-05-10 09:25:23 +0200

    Move `to_roman` in its own file.

--- src/lib/game/data/units/unit.cpp
+++ src/lib/game/data/units/unit.cpp
@@ -31,6 +31,7 @@
 #include "utility/listhelpers.h"
 #include "utility/mathtools.h"
 #include "utility/position.h"
+#include "utility/string/roman.h"
 
 #include <algorithm>
 
@@ -306,43 +307,6 @@ uint32_t cUnit::getChecksum (uint32_t crc) const
 cBox<cPosition> cUnit::getArea() const
 {
 	return cBox<cPosition> (position, position + (isBig ? cPosition (1, 1) : cPosition (0, 0)));
-}
-
-//------------------------------------------------------------------------------
-// http://rosettacode.org/wiki/Roman_numerals/Encode#C.2B.2B
-static std::string to_roman (unsigned int value)
-{
-	struct romandata_t
-	{
-		unsigned int value;
-		char const* numeral;
-	};
-	const struct romandata_t romandata[] =
-		{
-			//{1000, "M"}, {900, "CM"},
-			//{500, "D"}, {400, "CD"},
-			{100, "C"},
-			{90, "XC"},
-			{50, "L"},
-			{40, "XL"},
-			{10, "X"},
-			{9, "IX"},
-			{5, "V"},
-			{4, "IV"},
-			{1, "I"},
-			{0, nullptr} // end marker
-		};
-
-	std::string result;
-	for (const romandata_t* current = romandata; current->value > 0; ++current)
-	{
-		while (value >= current->value)
-		{
-			result += current->numeral;
-			value -= current->value;
-		}
-	}
-	return result;
 }
 
 //------------------------------------------------------------------------------
---
+++ src/lib/utility/string/roman.cpp
@@ -0,0 +1,57 @@
+/***************************************************************************
+ *      Mechanized Assault and Exploration Reloaded Projectfile            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "roman.h"
+
+//------------------------------------------------------------------------------
+// http://rosettacode.org/wiki/Roman_numerals/Encode#C.2B.2B
+std::string to_roman (unsigned int value)
+{
+	struct romandata_t
+	{
+		unsigned int value;
+		char const* numeral;
+	};
+	const struct romandata_t romandata[] =
+		{
+			//{1000, "M"}, {900, "CM"},
+			//{500, "D"}, {400, "CD"},
+			{100, "C"},
+			{90, "XC"},
+			{50, "L"},
+			{40, "XL"},
+			{10, "X"},
+			{9, "IX"},
+			{5, "V"},
+			{4, "IV"},
+			{1, "I"},
+			{0, nullptr} // end marker
+		};
+
+	std::string result;
+	for (const romandata_t* current = romandata; current->value > 0; ++current)
+	{
+		while (value >= current->value)
+		{
+			result += current->numeral;
+			value -= current->value;
+		}
+	}
+	return result;
+}
