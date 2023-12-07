c98ab12460165c6468045c7da9432de2e12d38bd
Jarod42 <Jarod42@users.noreply.github.com>
2023-12-07 21:52:50 +0100

    More checks in `utf8::decodeUnicode`

--- src/lib/utility/string/utf-8.cpp
+++ src/lib/utility/string/utf-8.cpp
@@ -21,6 +21,7 @@
 
 #include "utility/log.h"
 
+#include <algorithm>
 #include <stdexcept>
 
 namespace utf8
@@ -132,54 +133,50 @@ namespace utf8
 			throw ::std::out_of_range ("invalid position for decodeUnicode");
 		}
 		const ::std::uint32_t c0 = static_cast<unsigned char> (text[pos]);
-		if ((c0 & 0xF8) == 0xF0)
-		{
-			if (text.size() <= pos + 3)
+		const auto is_extra_char = [] (char c) { return (c & 0b1100'0000) == 0b1000'0000; };
+		const auto check_extra_size = [&] (int size) {
+			if (text.size() <= pos + size || !std::all_of (&text[pos + 1], &text.c_str()[pos + 1 + size], is_extra_char))
 			{
 				Log.warn ("Invalid UTF-8 string in text: '" + text + "' at pos " + ::std::to_string (pos));
 				throw ::std::out_of_range ("invalid position for decodeUnicode");
 			}
-			const unsigned char c1 = text[pos + 1] & 0x3F;
-			const unsigned char c2 = text[pos + 2] & 0x3F;
-			const unsigned char c3 = text[pos + 3] & 0x3F;
+		};
+		if ((c0 & 0b1111'1000) == 0b1111'0000)
+		{
+			check_extra_size (3);
+			const unsigned char c1 = text[pos + 1] & 0b0011'1111;
+			const unsigned char c2 = text[pos + 2] & 0b0011'1111;
+			const unsigned char c3 = text[pos + 3] & 0b0011'1111;
 
 			pos += 4;
-			return ((c0 & 0x07) << 18) | (c1 << 12) | (c2 << 6) | c3;
+			return ((c0 & 0b000'0111) << 18) | (c1 << 12) | (c2 << 6) | c3;
 		}
-		else if ((c0 & 0xF0) == 0xE0)
+		else if ((c0 & 0b1111'0000) == 0b1110'0000)
 		{
-			if (text.size() <= pos + 2)
-			{
-				Log.warn ("Invalid UTF-8 string in text: '" + text + "' at pos " + ::std::to_string (pos));
-				throw ::std::out_of_range ("invalid position for decodeUnicode");
-			}
-			const unsigned char c1 = text[pos + 1] & 0x3F;
-			const unsigned char c2 = text[pos + 2] & 0x3F;
+			check_extra_size (2);
+			const unsigned char c1 = text[pos + 1] & 0b0011'1111;
+			const unsigned char c2 = text[pos + 2] & 0b0011'1111;
 
 			pos += 3;
-			return ((c0 & 0x0F) << 12) | (c1 << 6) | c2;
+			return ((c0 & 0b0000'1111) << 12) | (c1 << 6) | c2;
 		}
-		else if ((c0 & 0xE0) == 0xC0)
+		else if ((c0 & 0b1110'0000) == 0b1100'0000)
 		{
-			if (text.size() <= pos + 1)
-			{
-				Log.warn ("Invalid UTF-8 string in text: '" + text + "' at pos " + ::std::to_string (pos));
-				throw ::std::out_of_range ("invalid position for decodeUnicode");
-			}
-			const unsigned char c1 = text[pos + 1] & 0x3F;
+			check_extra_size (1);
+			const unsigned char c1 = text[pos + 1] & 0b0011'1111;
 
 			pos += 2;
-			return ((c0 & 0x1F) << 6) | c1;
+			return ((c0 & 0b0001'1111) << 6) | c1;
 		}
 		else
 		{
-			if ((c0 & 0x80) != 0)
+			if ((c0 & 0b1000'0000) != 0)
 			{
 				Log.warn ("Invalid UTF-8 string in text: '" + text + "' at pos " + ::std::to_string (pos));
 				throw ::std::runtime_error ("Invalid utf8 character for decodeUnicode");
 			}
 			pos += 1;
-			return (c0 & 0x7F);
+			return (c0 & 0b0111'1111);
 		}
 	}
 
