d80ee58247ecb49c7af1ec3219b4cba652aa409b
Jarod42 <Jarod42@users.noreply.github.com>
2025-11-11 21:09:43 +0100

    [typo] engineere -> engineer

--- src/ui/translations.cpp
+++ src/ui/translations.cpp
@@ -498,7 +498,7 @@ std::string getStatusStr (const cVehicle& vehicle, const cPlayer* player, const 
 				}
 				return sText + dated;
 			}
-			else //small building is rdy + activate after engineere moves away
+			else // small building is ready + activate after engineer moves away
 			{
 				sText = lngPack.i18n ("Comp~Producing_Fin");
 				sText += lngPack.i18n ("Punctuation~Colon");
